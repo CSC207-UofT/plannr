55847c42423ae3df640c124ccbb9666579c9d507
hellokathylee <kathyam.lee@mail.utoronto.ca>
2021-10-10 17:57:38 -0400

    commit folders

---
+++ src/main/java/EventManager.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class EventManager{
+
+//    public boolean addEvent(String name, int priority, Date startDate, Date endDate) {
+//        Event event = new
+//    }
+
+}
