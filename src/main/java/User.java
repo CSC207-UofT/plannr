8896a1d085d42c053983c080493cd67260a537bb
hellokathylee <kathyam.lee@mail.utoronto.ca>
2021-10-11 19:15:42 -0400

    implemented User.java

---
+++ src/main/java/User.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class User {
+    private String name;
+    private ArrayList<String> courses;
+    private String school;
+    private ArrayList<Event> eventList;
+
+    /**
+     * Construct a User, giving them the given name,
+     * courses, school, and setting an empty events list.
+     *
+     * @param name      The User's name
+     * @param courses   The User's course list
+     * @param school    The User's school
+     * @param eventList The User's list of all events
+     */
+    public User(String name, ArrayList<String> courses, String school) {
+        this.name = name;
+        this.courses = courses;
+        this.school = school;
+        this.eventList = new ArrayList<Event>();
+    }
+
+    /**
+     * getter method for name
+     * @return a String that describes the User's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * getter method for courses
+     * @return an ArrayList that describes the User's current course list
+     */
+    public ArrayList<String> getCourses() {
+        return courses;
+    }
+
+    /**
+     * getter method for school
+     * @return a String that describes the User's school
+     */
+    public String getSchool() {
+        return school;
+    }
+
+    /**
+     * getter method for eventList
+     * @return an ArrayList that describes all the user's events
+     */
+    public ArrayList<Event> getEventList() {
+        return eventList;
+    }
+}
