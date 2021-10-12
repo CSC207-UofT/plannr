02d3adbd7676556b7824a8bf370fe4167402a8a1
dalshekerchi <d.alshekerchi@mail.utoronto,ca>
2021-10-11 21:54:36 -0400

    Finalized Deadline and Assessment

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,7 +1,29 @@
 import java.util.Date;
 
 public class Deadline extends Event {
-    public Deadline(String name, int priority, Date endDate) {
+    private String course; //course the deadline is from
+
+    /**
+     * Construct Deadline event, giving them the given
+     * name, priority, end date, and course name.
+     *
+     * @param name   This Deadline's name
+     * @param priority  This Deadline's priority
+     * @param endDate  This Deadline's due date
+     * @param courseName  This Deadline's course which it's from
+     */
+    public Deadline(String name, int priority, Date endDate, String courseName) {
         super(name, priority, endDate);
+        this.course = courseName;
+    }
+
+    /**
+     * toString method
+     * @return a String that describes this Deadline event.
+     */
+    @Override
+    public String toString() {
+        String result = "This assignment from " + this.course + " is due on " + this.getEndDate();
+        return result;
     }
 }
