1b1885c302e54a74cb582eb3fbd3041de874a94a
dalshekerchi <d.alshekerchi@mail.utoronto,ca>
2021-10-11 20:26:39 -0400

    Added getters and setters for instance variable

---
+++ src/main/java/StudySession.java
@@ -0,0 +1,54 @@
+import java.util.Date;
+
+public class StudySession {
+    private String course;
+    private String location;
+    private String notes;
+    private ArrayList participants;
+
+    /**
+     * Construct an Study Session, giving them the given course,
+     * location, notes, and participants.
+     *
+     * @param course         The Study Session's course
+     * @param location       The Study Session's location
+     * @param notes          The Study Session's notes
+     * @param participants   The Study Session's participants
+     */
+    public Event(String course, String location, String notes, ArrayList participants) {
+        this.course = course;
+        this.location = location;
+        this.notes = notes;
+        this.participants = participants;
+    /**
+     * Gets the course name of the Study Session
+     * @return the name of the course as a String
+     */
+    public String getCourse() {
+        return course;
+    }
+
+    /**
+     * Gets the location of the study session
+     * @return the location of the study session as a string
+     */
+    public String getLocation() {
+        return location;
+    }
+
+    /**
+     * Gets the notes of the study session
+     * @return the snotes of the study session as a string
+     */
+    public String getNotes() {
+        return notes;
+    }
+
+    /**
+     * Gets the participants of the study session
+     * @return the participants of the study session as an Array List
+     */
+    public ArrayList getParticipants() {
+        return participants;
+    }
+}
