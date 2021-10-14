8aaf43cebf30affc6d874cf51683f26e4b38c8e8
hellokathylee <kathyam.lee@mail.utoronto.ca>
2021-10-13 22:10:57 -0400

    commented out (to be implemented in phase 1) subclasses

--- src/main/java/Class.java
+++ src/main/java/Class.java
@@ -1,46 +1,48 @@
-import java.util.Date;
-
-public class Class extends Work {
-
-    private String course;
-    private String note;
-
-    /**
-     * Construct a Class event, giving it the given name,
-     * priority, start date, end date, the course code,
-     * the location, and a note.
-     *
-     * @param name      The Class's name
-     * @param priority  The Class's priority
-     * @param startDate The Class's start date
-     * @param endDate   The Class's end date
-     * @param course    The Class's course code
-     * @param location  The Class's location
-     * @param note      The Class's note
-     */
-    public Class(String name, int priority, Date startDate, Date endDate,
-                 String course, String location, String note) {
-        super(name, priority, startDate, endDate, location);
-        this.course = course;
-        this.note = note;
-    }
-
-    /**
-     * Gets the course of the Class event
-     * @return the course of the Class event as a String object
-     */
-    public String getCourse() { return this.course; }
-
-    /**
-     * Gets the notes of the Class event
-     * @return the note of the Class event as a String object
-     */
-    public String getNote() { return this.note; }
-
-    /**
-     * toString method
-     * @return a String that describes the Class event
-     */
-    @Override
-    public String toString() { return super.getName() + ": " + this.getCourse(); }
-}
+// TO BE IMPLEMENTED IN PHASE 1
+//
+// import java.time.LocalDate;
+//
+//public class Class extends Work {
+//
+//    private String course;
+//    private String note;
+//
+//    /**
+//     * Construct a Class event, giving it the given name,
+//     * priority, start date, end date, the course code,
+//     * the location, and a note.
+//     *
+//     * @param name      The Class's name
+//     * @param priority  The Class's priority
+//     * @param startDate The Class's start date
+//     * @param endDate   The Class's end date
+//     * @param course    The Class's course code
+//     * @param location  The Class's location
+//     * @param note      The Class's note
+//     */
+//    public Class(String name, int priority, Date startDate, Date endDate,
+//                 String course, String location, String note) {
+//        super(name, priority, startDate, endDate, location);
+//        this.course = course;
+//        this.note = note;
+//    }
+//
+//    /**
+//     * Gets the course of the Class event
+//     * @return the course of the Class event as a String object
+//     */
+//    public String getCourse() { return this.course; }
+//
+//    /**
+//     * Gets the notes of the Class event
+//     * @return the note of the Class event as a String object
+//     */
+//    public String getNote() { return this.note; }
+//
+//    /**
+//     * toString method
+//     * @return a String that describes the Class event
+//     */
+//    @Override
+//    public String toString() { return super.getName() + ": " + this.getCourse(); }
+//}
--- src/main/java/Social.java
+++ src/main/java/Social.java
@@ -1,85 +1,87 @@
-import java.util.ArrayList;
-import java.util.Date;
-
-public class Social extends Event{
-    private int duration;
-    private String location;
-    private ArrayList<String> participants;
-
-    /**
-     * Social events class constructor, which calls its super class Event
-     * This constructor LETS THE USER PROVIDE PARTICIPANTS
-     * @param name          Name of event
-     * @param priority      Priority of event
-     * @param startDate     Start date of event
-     * @param endDate       End date of event
-     * @param duration      Duration of event
-     * @param location      Location of event
-     * @param participants  Participants of event
-     */
-    public Social(String name, int priority, Date startDate, Date endDate,
-                  int duration, String location, ArrayList<String> participants) {
-        // Calls the super method
-        super(name, priority, startDate, endDate);
-        // Assigns instance variables unique to Social
-        this.duration = duration;
-        this.location = location;
-        this.participants = participants;
-    }
-
-    /**
-     * Social events class constructor, which calls its super class Event
-     * This constructor DOES NOT REQUIRE USER TO INPUT LIST OF PARTICIPANTS
-     * @param name          Name of event
-     * @param priority      Priority of event
-     * @param startDate     Start date of event
-     * @param endDate       End date of event
-     * @param duration      Duration of event
-     * @param location      Location of event
-     */
-    public Social(String name, int priority, Date startDate, Date endDate,
-                  int duration, String location) {
-        // Calls the super method
-        super(name, priority, startDate, endDate);
-        // Assigns instance variables unique to Social
-        this.duration = duration;
-        this.location = location;
-        this.participants = new ArrayList<String>();
-    }
-
-    /**
-     * Gets the duration of this Social event
-     * @return the duration of this Social event
-     */
-    public int getDuration() {
-        return this.duration;
-    }
-
-    /** ========== SUBJECT TO CHANGE ============
-     * toString method for Social, used for printing to screen
-     * @return a String that describes this Social event
-     */
-    @Override
-    public String toString() {
-        String result = (
-                "The event "
-                + this.getName()
-                + " is scheduled to start on "
-                + this.getStartDate()
-                + " and end on "
-                + this.getEndDate()
-                + ". It would last "
-                + this.getDuration()
-                + " hours. \n The expected participants are: "
-        );
-
-        // Use a for loop to append the participatns into the result String
-        if (this.participants.size() > 0) {
-            for (String p:this.participants) {
-                result += p + "\n";
-            }
-        }
-
-        return result;
-    }
-}
+// TO BE IMPLEMENTED IN PHASE 1
+//
+// import java.util.ArrayList;
+//import java.util.Date;
+//
+//public class Social extends Event{
+//    private int duration;
+//    private String location;
+//    private ArrayList<String> participants;
+//
+//    /**
+//     * Social events class constructor, which calls its super class Event
+//     * This constructor LETS THE USER PROVIDE PARTICIPANTS
+//     * @param name          Name of event
+//     * @param priority      Priority of event
+//     * @param startDate     Start date of event
+//     * @param endDate       End date of event
+//     * @param duration      Duration of event
+//     * @param location      Location of event
+//     * @param participants  Participants of event
+//     */
+//    public Social(String name, int priority, Date startDate, Date endDate,
+//                  int duration, String location, ArrayList<String> participants) {
+//        // Calls the super method
+//        super(name, priority, startDate, endDate);
+//        // Assigns instance variables unique to Social
+//        this.duration = duration;
+//        this.location = location;
+//        this.participants = participants;
+//    }
+//
+//    /**
+//     * Social events class constructor, which calls its super class Event
+//     * This constructor DOES NOT REQUIRE USER TO INPUT LIST OF PARTICIPANTS
+//     * @param name          Name of event
+//     * @param priority      Priority of event
+//     * @param startDate     Start date of event
+//     * @param endDate       End date of event
+//     * @param duration      Duration of event
+//     * @param location      Location of event
+//     */
+//    public Social(String name, int priority, Date startDate, Date endDate,
+//                  int duration, String location) {
+//        // Calls the super method
+//        super(name, priority, startDate, endDate);
+//        // Assigns instance variables unique to Social
+//        this.duration = duration;
+//        this.location = location;
+//        this.participants = new ArrayList<String>();
+//    }
+//
+//    /**
+//     * Gets the duration of this Social event
+//     * @return the duration of this Social event
+//     */
+//    public int getDuration() {
+//        return this.duration;
+//    }
+//
+//    /** ========== SUBJECT TO CHANGE ============
+//     * toString method for Social, used for printing to screen
+//     * @return a String that describes this Social event
+//     */
+//    @Override
+//    public String toString() {
+//        String result = (
+//                "The event "
+//                + this.getName()
+//                + " is scheduled to start on "
+//                + this.getStartDate()
+//                + " and end on "
+//                + this.getEndDate()
+//                + ". It would last "
+//                + this.getDuration()
+//                + " hours. \n The expected participants are: "
+//        );
+//
+//        // Use a for loop to append the participatns into the result String
+//        if (this.participants.size() > 0) {
+//            for (String p:this.participants) {
+//                result += p + "\n";
+//            }
+//        }
+//
+//        return result;
+//    }
+//}
