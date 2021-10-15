370e4e84362f4f8026ebbf92a3a41d7a56ddb734
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-10-15 12:00:08 -0400

    implemented getUserInfo

--- src/main/java/SystemInOut.java
+++ src/main/java/SystemInOut.java
@@ -1,2 +1,63 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
 public class SystemInOut {
+    Scanner sc;
+
+    /* Constructor for the controller
+    This constructor takes no argument and creates a scanner within the controller
+     */
+    public SystemInOut() {
+        this.sc = new Scanner(System.in);
+    }
+
+    /* This method will take in user info to construct the user*/
+    public void userOnBoard() {
+        // Welcome message
+        System.out.println("Welcome to Uni Life Tracker!\nWe are here to help you track your disastrous life!");
+        System.out.println("-------------------------");
+        // Get user's info
+        ArrayList<String> user_info = this.getUserInfo();
+    }
+
+    /* This method will get the user's name, while catching for errors */
+    private ArrayList<String> getUserInfo() {
+        ArrayList<String> result = new ArrayList<>();
+        // Get user's name
+        String name = "";
+        do {
+            System.out.println("What's your name?");
+            try {
+                name = sc.nextLine();
+                // Catch empty strings
+                if (name.length() == 0) {
+                    System.out.println("Please enter a name.");
+                }
+            } catch (Exception e) { // Catch for invalid input
+                System.out.println("Invalid input, please try again.");
+            }
+        } while (name.length() == 0);
+        result.add(name);
+        // Get user's university
+        String uni = "";
+        do {
+            System.out.println("What's name of the school you attend?");
+            try {
+                uni = sc.nextLine();
+                // Catch empty strings
+                if (uni.length() == 0) {
+                    System.out.println("Please enter a name.");
+                }
+            } catch (Exception e) { // Catch for invalid input
+                System.out.println("Invalid input, please try again.");
+            }
+        } while (uni.length() == 0);
+        result.add(uni);
+
+        return result;
+    }
+
 }
