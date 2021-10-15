7cda0c5a9e3bb590e7e490391e4faea645a0310a
cdw18 <b.amoussou@mail.utoronto.ca>
2021-10-14 20:58:13 -0400

    - Creating DeadlineTest - Removed some comments from UniLifeTracker

--- src/main/java/EventManager.java
+++ src/main/java/EventManager.java
@@ -1,27 +1,27 @@
-import java.util.ArrayList;
-import java.util.Date;
-
-public class EventManager{
-
-    /**
-     * Add an Event
-     *
-     * @param e    The Event
-     * @param user  The User
-     */
-    public void addEvent(Event e, User user){
-        user.getEventList().add(e);
-    }
-
-    /**
-     * Delete an Event
-     *
-     * @param e     The Event
-     * @param user  The User
-     */
-    public boolean deleteEvent(Event e, User user){
-        return user.getEventList().remove(e);
-    }
-
-
-}
+//import java.util.ArrayList;
+//import java.util.Date;
+//
+//public class EventManager{
+//
+//    /**
+//     * Add an Event
+//     *
+//     * @param e    The Event
+//     * @param user  The User
+//     */
+//    public void addEvent(Event e, User user){
+//        user.getEventList().add(e);
+//    }
+//
+//    /**
+//     * Delete an Event
+//     *
+//     * @param e     The Event
+//     * @param user  The User
+//     */
+//    public boolean deleteEvent(Event e, User user){
+//        return user.getEventList().remove(e);
+//    }
+//
+//
+//}
--- src/test/DeadlineTest.java
+++ src/test/DeadlineTest.java
@@ -1,4 +1,9 @@
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 class DeadlineTest {
 
