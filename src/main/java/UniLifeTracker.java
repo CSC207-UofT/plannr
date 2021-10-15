c33f0cbf7b8fa7d6bd1df78a4808e668edd8a23a
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-10-15 16:14:28 -0400

    cleaned up main method

--- src/main/java/UniLifeTracker.java
+++ src/main/java/UniLifeTracker.java
@@ -1,17 +1,6 @@
-
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class UniLifeTracker {
 
     public static void main (String[] args) {
-        //Setting things up
-        Scanner in = new Scanner(System.in);
-        Scanner scan = new Scanner(System.in);
-        DateTimeFormatter customFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm a");
-
         // Onboard user
         UserManager userManager = SystemInOut.onBoardUser();
         // run the program
