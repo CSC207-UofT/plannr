8fb2de533c5c0caf8d3c92c5c51a58800a1a7c8e
cdw18 <b.amoussou@mail.utoronto.ca>
2021-10-14 20:33:47 -0400

    Changes by kathy

--- src/main/java/UserManager.java
+++ src/main/java/UserManager.java
@@ -20,8 +20,11 @@ public class UserManager {
         return u.getEventList();
     }
 
+    public ArrayList<String> viewCourses() {
+        return u.getCourses();
+    }
 
-    @Override
+        @Override
     public String toString() {
         return "bob";
     }
