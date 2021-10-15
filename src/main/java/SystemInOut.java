8d2924f08665ab389fc6b860d51c7fa429aa0f0a
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-10-15 15:58:22 -0400

    finished implementing run method

--- src/main/java/SystemInOut.java
+++ src/main/java/SystemInOut.java
@@ -1,21 +1,24 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SystemInOut {
     private static final Scanner sc = new Scanner(System.in);
-
+    public static final DateTimeFormatter DATEFORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
     /**
      * This method will create the User
      * @return an UserManager object
      */
-    public static UserManager userOnBoard() {
+    public static UserManager onBoardUser() {
         // Welcome message
         System.out.println("Welcome to Uni Life Tracker!\nWe are here to help you track your disastrous life!");
         System.out.println("-------------------------");
         // Get user's info
-        ArrayList<String> userInfo = getUserInfo();
+        ArrayList<String> userInfo = getUserInfoDialog();
         // Get user's courses
-        ArrayList<String> userCourses = getUserCourses();
+        ArrayList<String> userCourses = getUserCoursesDialog();
 
         System.out.println("-------------------------");
 
@@ -26,7 +29,7 @@ public class SystemInOut {
      * This method will get the user's name, while catching for errors
      * helper of userOnBoard
      */
-    private static ArrayList<String> getUserInfo() {
+    private static ArrayList<String> getUserInfoDialog() {
         ArrayList<String> result = new ArrayList<>();
         // Get user's name
         String name = "";
@@ -66,7 +69,7 @@ public class SystemInOut {
      * This method will get user's courses and return an ArrayList
      * helper of userOnBoard
      */
-    private static ArrayList<String> getUserCourses() {
+    private static ArrayList<String> getUserCoursesDialog() {
         ArrayList<String> courses = new ArrayList<>();
         // Prompt user
         System.out.print("What classes are you attending? If you don't have any courses at the moment, please " +
@@ -112,7 +115,7 @@ public class SystemInOut {
             // User chose to create new event, calls helper and set nextLine back to 0 to continue the loop
             // NOTE: Future actions will be dealt with here using the if statements
             if (nextLine == 1) {
-                addEvent(userManager);
+                addEventDialog(userManager);
                 nextLine = 0; // this allows us to loop until the user chooses to exit, which sets it to 2
             }
 
@@ -123,7 +126,57 @@ public class SystemInOut {
      * This deals with everything related to the inputs for creating events during runtime
      * @param userManager the manager of the current user
      */
-    private static void addEvent(UserManager userManager) {
+    private static void addEventDialog(UserManager userManager) {
+        // Condition for while loop
+        boolean stay = true;
+        // do-while loop to prompt user for input
+        do {
+            // Provide options
+            System.out.println("Please choose the type of event you wish to create:");
+            System.out.println("1. Return to main menu" +
+                    "\n2. Deadline");
+            // Get user input and catch for errors
+            try {
+                int nextLine = sc.nextInt();
+                sc.nextLine(); // To consume the next line character so that our next "nextLine" can function properly
+                // exit current menu if user wants to return
+                if (nextLine != 1) {
+                    // Ask for general properties of an event
+                    // Get name of event
+                    System.out.println("Enter the name of the event: ");
+                    String eventName = sc.nextLine();
 
+                    // Get endDate of event
+                    System.out.println("Enter the due date (format: \"dd-mm-yyyy HH:mm\")");
+                    String eventDDL = sc.nextLine();
+                    LocalDateTime eventEndDate = LocalDateTime.parse(eventDDL, DATEFORMAT);
+                    // Get priority of event
+                    System.out.println("Enter the priority of the event (0 = high, 1 = mid, 2 = low): ");
+                    int eventPriority = sc.nextInt();
+
+                    // Ask for deadline specific properties
+                    if (nextLine == 2) {
+                        // Print out all the courses the user currently has
+                        for (int i = 0; i < userManager.viewCourses().size(); i++) {
+                            System.out.println((i+1) + ". " + userManager.viewCourses().get(i));
+                        }
+                        System.out.println("Select the course this deadline is for (enter the corresponding number");
+                        int ddlCourseNum = sc.nextInt();
+                        // Get course name
+                        String ddlCourseName = userManager.viewCourses().get(ddlCourseNum-1);
+                        // Create and add deadline event object using UserManager
+                        userManager.addDeadlineEvent(eventName, eventPriority, eventEndDate, ddlCourseName);
+                    }
+                } else{ stay = false;}
+            } catch (Exception e) {
+                System.out.println("Invalid input, please try again.");
+            }
+            // Print all the user's events with their details
+            System.out.println("Your current events and deadlines: ");
+            for (Event e : userManager.viewEventList()) {
+                System.out.println(e);
+            }
+            System.out.println("-------------------------");
+        } while (stay);
     }
 }
