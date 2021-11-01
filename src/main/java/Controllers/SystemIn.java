6fa7f9a2ed9ee91b7652b3c27df6c886cdf004a2
OKok-3 <daniel.guan103@gmail.com>
2021-11-01 01:50:08 -0400

    Separated userOnboard

--- src/main/java/Controllers/SystemIn.java
+++ src/main/java/Controllers/SystemIn.java
@@ -1,4 +1,45 @@
 package Controllers;
 
+import Gateways.SystemOut;
+import UseCases.UserManager;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SystemIn {
+    // Create a static scanner
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static UserManager onBoardUser() {
+        // Get user's information by invoking the getUserInfoDialog
+        ArrayList<String> info = SystemOut.getUserInfoDialog();
+        // Get user's courses by invoking the getUserCoursesDialog
+        ArrayList<String> courses = SystemOut.getUserCoursesDialog();
+
+        return new UserManager(info.get(0), courses, info.get(1));
+    }
+
+    /**
+     * This method will get String inputs from the user
+     * @return returns the input, else returns "Invalid Input"
+     */
+    public static String getStrInput() {
+        try {
+            return sc.nextLine();
+        } catch(Exception e) {
+            return "Invalid";
+        }
+    }
+
+    /**
+     * This method will get String inputs from the user
+     * @return Returns the input, else returns -1 for invalid inputs
+     */
+    public static int getIntInput() {
+        try {
+            return sc.nextInt();
+        } catch (Exception e) {
+            return -1;
+        }
+    }
 }
--- src/main/java/Gateways/SystemOut.java
+++ src/main/java/Gateways/SystemOut.java
@@ -1,4 +1,81 @@
 package Gateways;
 
+import Controllers.SystemIn;
+
+import java.util.ArrayList;
+
 public class SystemOut {
+    /**
+     * This method will print the welcome message of this program
+     */
+    public static void welcomeMessage() {
+        // Welcome message
+        System.out.println("--- WELCOME TO UNI LIFE TRACKER! ---" +
+                "\nWe are here to help you track your disastrous life!");
+        System.out.println("-------------------------");
+    }
+
+    /**
+     * This will print out the dialog for the user to input personal info
+     * @return returns an ArrayList that contains the user's name and school name, in this respective order
+     */
+    public static ArrayList<String> getUserInfoDialog() {
+        ArrayList<String> result = new ArrayList<>();
+        // Empty variable to store user's name
+        String name = "";
+        // While loop to keep asking for input if invalid or empty
+        while (name.length() == 0 || name.equals("Invalid")) {
+            System.out.println("What's your name?");
+            name = SystemIn.getStrInput();
+            if (name.equals("Invalid")) {
+                System.out.println("Invalid input, please try again");
+            }
+        }
+        result.add(name);
+
+        // Empty variable to store user's school information
+        String school = "";
+        // Same logic as before
+        while (school.length() == 0 || school.equals("Invalid")) {
+            System.out.println("What's the name of the school you are attending?");
+            school = SystemIn.getStrInput();
+            if (school.equals("Invalid")) {
+                System.out.println("Invalid input, please try again");
+            }
+        }
+        result.add(school);
+
+        return result;
+    }
+
+    /**
+     * This will print out the dialog for the user to input courses
+     * @return an ArrayList<String> list of courses
+     */
+    public static ArrayList<String> getUserCoursesDialog() {
+        // Empty ArrayList to store user's courses
+        ArrayList<String> courses = new ArrayList<>();
+        // Empty String to store user's input
+        String input = "";
+        // Prompt user
+        System.out.print("What classes are you attending? If you don't have any courses at the moment, please " +
+                "type n.\n" + "Please enter the course ID: ");
+        while (!input.equals("n")) {
+            // Get user's input
+            input = SystemIn.getStrInput();
+            // Check if String is invalid or empty
+            if (input.equals("Invalid") || input.length() == 0) {
+                System.out.println("Invalid input, please try again.");
+            } else if (courses.contains(input)) { // check if course already added
+                System.out.println("Course already added, please try another one.");
+            } else {
+                courses.add(input);
+                System.out.println("Course \"" + input + "\" added!");
+            }
+            System.out.println("Please enter the course ID or type n to stop.");
+        }
+
+        return courses;
+    }
+
 }
