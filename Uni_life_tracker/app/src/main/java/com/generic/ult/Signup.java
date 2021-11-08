1499785074a490051ed9efbbb908b6d3d9d27e13
dalshekerchi <d.alshekerchi@mail.utoronto,ca>
2021-11-07 23:52:22 -0500

    Created button to get to the signup page

---
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/Signup.java
@@ -0,0 +1,13 @@
+package com.generic.ult;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
