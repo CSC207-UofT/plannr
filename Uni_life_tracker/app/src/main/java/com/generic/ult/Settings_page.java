3f49fb8161fab1f87d395733621ad2305368c9a5
EvgeniaSila <evgenia.silajev@mail.utoronto.ca>
2021-11-12 17:32:35 -0500

    commented out code for later use in Settings_page.java

--- Uni_life_tracker/app/src/main/java/com/generic/ult/Settings_page.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/Settings_page.java
@@ -1,45 +1,44 @@
 package com.generic.ult;
-import android.view.View;
+// import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.google.android.material.textfield.TextInputLayout;
+// import com.google.android.material.textfield.TextInputLayout;
 
-import java.util.Objects;
+// import java.util.Objects;
 
 
 public class Settings_page extends AppCompatActivity {
 
 
-    private TextInputLayout textInputName;
-    private TextInputLayout textInputUni;
+//    private TextInputLayout textInputName;
+//    private TextInputLayout textInputUni;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_page);
-        textInputName = findViewById(R.id.settings_name);
-        textInputUni = findViewById(R.id.settings_uni);
+//        textInputName = findViewById(R.id.settings_name);
+//        textInputUni = findViewById(R.id.settings_uni);
     }
 
-    private boolean validate(TextInputLayout textInput) {
-        String Input = Objects.requireNonNull(textInput.getEditText()).getText().toString().trim();
-
-        if (Input.isEmpty()) {
-            textInput.setError("Field cannot be empty");
-            return false;
-        } else {
-            textInput.setError(null);
-            return true;
-        }
+//    private boolean validate(TextInputLayout textInput) {
+//        String Input = Objects.requireNonNull(textInput.getEditText()).getText().toString().trim();
+//
+//        if (Input.isEmpty()) {
+//            textInput.setError("Field cannot be empty");
+//            return false;
+//        } else {
+//            textInput.setError(null);
+//            return true;
+//        }
     }
 
-    public void settingsInput(View v) {
-        if (!(validate(textInputName) | validate(textInputUni))) {
-            // Here we can get all the info we need
-
-        }
-
-
-    }
-}
+//    public void settingsInput(View v) {
+//        if (!(validate(textInputName) | validate(textInputUni))) {
+//            // Here we can get all the info we need
+//
+//        }
+//
+//
+//    }
