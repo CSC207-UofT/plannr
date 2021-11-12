3cc7810e9d9b7aaad7b633f4f44af73f708ba6ce
EvgeniaSila <evgenia.silajev@mail.utoronto.ca>
2021-11-12 17:52:07 -0500

    removed unused fragment files and commented out code in expenses_landing.java and Add_expenses.java

--- Uni_life_tracker/app/src/main/java/com/generic/ult/expenses_landing.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/expenses_landing.java
@@ -6,29 +6,29 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import com.generic.ult.databinding.ActivityExpensesLandingBinding;
+//import com.generic.ult.databinding.ActivityExpensesLandingBinding;
 import com.google.android.material.snackbar.Snackbar;
 
 public class expenses_landing extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        com.generic.ult.databinding.ActivityExpensesLandingBinding binding = ActivityExpensesLandingBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        //setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_expenses_landing);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.eGoToViewButton.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        com.generic.ult.databinding.ActivityExpensesLandingBinding binding = ActivityExpensesLandingBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        //setSupportActionBar(binding.toolbar);
+//
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_expenses_landing);
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//
+//        binding.eGoToViewButton.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show());
+    //}
 
     @Override
     public boolean onSupportNavigateUp() {
