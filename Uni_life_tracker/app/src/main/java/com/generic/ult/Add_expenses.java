de796b30e063e1b954fbca062a7dba957486f90e
EvgeniaSila <evgenia.silajev@mail.utoronto.ca>
2021-11-13 17:43:23 -0500

    removed unused code

--- Uni_life_tracker/app/src/main/java/com/generic/ult/Add_expenses.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/Add_expenses.java
@@ -13,27 +13,13 @@ public class Add_expenses extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        com.generic.ult.databinding.ActivityAddExpensesBinding binding = ActivityAddExpensesBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//
-//       // setSupportActionBar(binding.toolbar);
-//
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_add_expenses);
-//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//
-//        binding.expenseAmount.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show());
-//    }
-
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_add_expenses);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+   }
+
+
+
 }
--- Uni_life_tracker/app/src/main/java/com/generic/ult/expenses_landing.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/expenses_landing.java
@@ -13,27 +13,8 @@ public class expenses_landing extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        com.generic.ult.databinding.ActivityExpensesLandingBinding binding = ActivityExpensesLandingBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//
-//        //setSupportActionBar(binding.toolbar);
-//
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_expenses_landing);
-//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//
-//        binding.eGoToViewButton.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show());
-    //}
-
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_expenses_landing);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+}
 }
