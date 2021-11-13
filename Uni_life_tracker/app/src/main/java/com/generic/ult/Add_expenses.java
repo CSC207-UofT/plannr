d776f8cfa9273383390dc59531c0465b87c53b22
EvgeniaSila <evgenia.silajev@mail.utoronto.ca>
2021-11-12 19:04:40 -0500

    fixed error message

--- Uni_life_tracker/app/src/main/java/com/generic/ult/Add_expenses.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/Add_expenses.java
@@ -2,12 +2,8 @@ package com.generic.ult;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import com.generic.ult.databinding.ActivityAddExpensesBinding;
-import com.google.android.material.snackbar.Snackbar;
+
 
 public class Add_expenses extends AppCompatActivity {
 
@@ -16,24 +12,9 @@ public class Add_expenses extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expenses);
 
-        com.generic.ult.databinding.ActivityAddExpensesBinding binding = ActivityAddExpensesBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-       // setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_add_expenses);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.expenseAmount.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_add_expenses);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+
 }
--- Uni_life_tracker/app/src/main/java/com/generic/ult/expenses_landing.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/expenses_landing.java
@@ -2,12 +2,8 @@ package com.generic.ult;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import com.generic.ult.databinding.ActivityExpensesLandingBinding;
-import com.google.android.material.snackbar.Snackbar;
+
 
 public class expenses_landing extends AppCompatActivity {
 
@@ -16,24 +12,7 @@ public class expenses_landing extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expenses_landing);
 
-        com.generic.ult.databinding.ActivityExpensesLandingBinding binding = ActivityExpensesLandingBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        //setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_expenses_landing);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.eGoToViewButton.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_expenses_landing);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
