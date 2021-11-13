6e8eac26076539aa899acfc56515da26eff949ab
EvgeniaSila <evgenia.silajev@mail.utoronto.ca>
2021-11-12 20:45:00 -0500

    moved text input to the side

--- Uni_life_tracker/app/src/main/java/com/generic/ult/Settings_page.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/Settings_page.java
@@ -1,44 +1,19 @@
 package com.generic.ult;
-// import android.view.View;
+
+import android.content.Intent;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-// import com.google.android.material.textfield.TextInputLayout;
 
-// import java.util.Objects;
 
 
 public class Settings_page extends AppCompatActivity {
-
-
-//    private TextInputLayout textInputName;
-//    private TextInputLayout textInputUni;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_page);
-//        textInputName = findViewById(R.id.settings_name);
-//        textInputUni = findViewById(R.id.settings_uni);
-    }
 
-//    private boolean validate(TextInputLayout textInput) {
-//        String Input = Objects.requireNonNull(textInput.getEditText()).getText().toString().trim();
-//
-//        if (Input.isEmpty()) {
-//            textInput.setError("Field cannot be empty");
-//            return false;
-//        } else {
-//            textInput.setError(null);
-//            return true;
-//        }
     }
-
-//    public void settingsInput(View v) {
-//        if (!(validate(textInputName) | validate(textInputUni))) {
-//            // Here we can get all the info we need
-//
-//        }
-//
-//
-//    }
+}
