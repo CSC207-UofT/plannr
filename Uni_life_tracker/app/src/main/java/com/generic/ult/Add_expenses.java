f51bb5432d5ab7f0664a7291069f8122cc99528c
EvgeniaSila <evgenia.silajev@mail.utoronto.ca>
2021-11-13 12:32:43 -0500

    added code to validate Add_expenses input

--- Uni_life_tracker/app/src/main/java/com/generic/ult/Add_expenses.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/Add_expenses.java
@@ -1,20 +1,56 @@
 package com.generic.ult;
 
 import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.AppBarConfiguration;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
 
 
 public class Add_expenses extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
 
+    private TextInputLayout textInputName;
+    private TextInputLayout textInputAmount;
+    private TextInputLayout textInputDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expenses);
 
+        textInputName = findViewById(R.id.add_e_name);
+        textInputAmount = findViewById(R.id.add_e_amount);
+        textInputDate = findViewById(R.id.add_e_date);
+
+
     }
+
+
+    private boolean validate(TextInputLayout textInput) {
+        String Input = Objects.requireNonNull(textInput.getEditText()).getText().toString().trim();
+
+        if (Input.isEmpty()) {
+            textInput.setError("Field cannot be empty");
+            return false;
+        } else {
+            textInput.setError(null);
+            return true;
+        }
+    }
+    public void signupInput(View v) {
+        if (!(validate(textInputName) | validate( textInputAmount) |
+                validate(textInputDate))) {
+            // Here we can get all the info we need
+            // For example to get the email you can do textInputEmail.getEditTest().getText().toString()
+        }
+
+
+    }
+
 
 
 }
