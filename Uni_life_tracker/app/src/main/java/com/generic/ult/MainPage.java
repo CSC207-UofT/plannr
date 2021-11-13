804d6ce6972504e7ba8638ddf8d1c424a32dbc73
hellokathylee <kathyam.lee@mail.utoronto.ca>
2021-11-13 10:47:39 -0500

    - implement events list

--- Uni_life_tracker/app/src/main/java/com/generic/ult/MainPage.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/MainPage.java
@@ -1,5 +1,6 @@
 package com.generic.ult;
 
+import Entities.Event;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
@@ -8,13 +9,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.text.DateFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Calendar;
 
-public class main_page extends AppCompatActivity {
+public class MainPage extends AppCompatActivity {
     // initialize variable
     DrawerLayout drawerLayout;
+
+    private ArrayList<Event> eventsList;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +37,29 @@ public class main_page extends AppCompatActivity {
         TextView textViewDate = findViewById(R.id.text_date);
         textViewDate.setText(currentDate);
 
+        // side menu
         drawerLayout = findViewById(R.id.drawer_layout);
-
+        
+        // events list
+        recyclerView = findViewById(R.id.recyclerView);
+        eventsList = new ArrayList<>(); 
+        setEventInfo();
+        setAdapter();
     }
 
+    private void setAdapter() {
+        recyclerAdapter adapter = new recyclerAdapter(eventsList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setEventInfo() {
+        eventsList.add(new Event("Event 1", 1, LocalDateTime.of(2019, 3, 28, 14, 33, 48)));
+        eventsList.add(new Event("Event 2", 2, LocalDateTime.of(2021, 12, 13, 12, 20, 48)));
+    }
+//    public Event(String name, int priority, LocalDateTime startDate, LocalDateTime endDate) {
     public void ClickMenu(View view){
         // open drawer
         openDrawer(drawerLayout);
@@ -61,15 +89,20 @@ public class main_page extends AppCompatActivity {
         recreate();
     }
 
-//    public void ClickDashboard(View view) {
-//        // redirect activity to dashboard
+    public void ClickLife(View view) {
+        // redirect activity to dashboard
+        redirectActivity(this, Signup.class); // change this to life later
+    }
+
+    public void ClickExpenses(View view) {
+        // redirect activity to dashboard
 //        redirectActivity(this,);
-//    }
-//
-////    public void ClickLife(View view) {
-////        // redirect activity to dashboard
-////        redirectActivity(this,);
-//    }
+    }
+
+    public void ClickSettings(View view) {
+        // redirect activity to dashboard
+//        redirectActivity(this,);
+    }
 
     public static void redirectActivity(Activity activity, Class aClass) {
         // initialize intent
