12c710c907a9f117f0fb9fc8e711a5cf7c350f76
EvgeniaSila <evgenia.silajev@mail.utoronto.ca>
2021-11-14 16:24:53 -0500

    fixed formatting issues and removed code that caused bug

--- Uni_life_tracker/app/src/main/java/com/generic/ult/AddExpenses.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/AddExpenses.java
@@ -13,42 +13,13 @@ public class AddExpenses extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
 
-    private TextInputLayout textInputName;
-    private TextInputLayout textInputAmount;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expenses);
 
-        textInputName = findViewById(R.id.add_e_name);
-        textInputAmount = findViewById(R.id.add_e_amount);
-
-
     }
-
-
-    private boolean validate(TextInputLayout textInput) {
-        String Input = Objects.requireNonNull(textInput.getEditText()).getText().toString().trim();
-
-        if (Input.isEmpty()) {
-            textInput.setError("Field cannot be empty");
-            return false;
-        } else {
-            textInput.setError(null);
-            return true;
-        }
-    }
-    public void signupInput(View v) {
-        if (!(validate(textInputName) | validate( textInputAmount))) {
-            // Here we can get all the info we need
-            // For example to get the email you can do textInputEmail.getEditTest().getText().toString()
-        }
-
-
-    }
-
 
 
 }
