afa862fccb6e400440d3e58a32c47062b4ed05b6
cdw18 <b.amoussou@mail.utoronto.ca>
2021-11-13 23:31:37 -0500

    Finished implementing Deadline and made DATEFORMAT in Event public.

--- Uni_life_tracker/app/src/main/java/Entities/Deadline.java
+++ Uni_life_tracker/app/src/main/java/Entities/Deadline.java
@@ -3,7 +3,6 @@ package Entities;
 import Entities.Event;
 import androidx.annotation.NonNull;
 
-import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 
 
@@ -11,8 +10,10 @@ import java.time.LocalDateTime;
  * Represents an event that is a deadline with a due date.
  */
 public class Deadline extends Event {
+    private String name;
+    private int priority;
+    private LocalDateTime endDate;
     private String course; //course the deadline is from
-    DateTimeFormatter customFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm a");
 
     /**
      * Construct Entities.Deadline event, giving them the given
@@ -24,13 +25,23 @@ public class Deadline extends Event {
      * @param courseName  This Entities.Deadline's course which it's from
      */
     public Deadline(String name, int priority, LocalDateTime endDate, String courseName) {
-        super(name, priority, endDate);
+        super(name, priority, endDate, endDate);
         this.course = courseName;
     }
 
+    /**
+     * Gets the course this deadline is for
+     * @return the course of this deadline event
+     */
     public String getCourse() {
         return this.course;
     }
+
+    /**
+     * Changes the course name this deadline is for
+     * @param course is the new name of the course for this deadline event
+     */
+    public void setCourse (String course) { this.course = course; }
 
     /**
      * toString method
@@ -51,6 +62,6 @@ public class Deadline extends Event {
         }
 
         return String.format("Deadline (%s priority): The assignment %s from %s is due on %s",
-                strPriority, this.getName(), this.getCourse(), this.getEndDate().format(customFormat));
+                strPriority, this.getName(), this.getCourse(), this.getEndDate().format(DATEFORMAT));
     }
 }
