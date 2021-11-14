3fd0f3ce21de955436fa9d9b7c013b76ea1acbed
hellokathylee <kathyam.lee@mail.utoronto.ca>
2021-11-14 03:12:35 -0500

    - implement settings back button

--- Uni_life_tracker/app/src/main/java/com/generic/ult/SettingsActivity.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/SettingsActivity.java
@@ -1,17 +1,25 @@
 package com.generic.ult;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-
-
 public class SettingsActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        ImageView backBtn = findViewById(R.id.back);
+        backBtn.setOnClickListener(v -> ClickBack(v));
+    }
+
+    public void ClickBack(View view) {
+        Intent intent = new Intent(this, MainPageActivity.class);
+        startActivity(intent);
     }
 }
