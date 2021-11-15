738558d265442eb501e56978b1d46370618e4ec0
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-11-15 16:17:12 -0500

    fixed javadocs

--- Uni_life_tracker/app/src/main/java/Entities/Expenses.java
+++ Uni_life_tracker/app/src/main/java/Entities/Expenses.java
@@ -6,13 +6,13 @@ public class Expenses {
     private String name;
     private double value;
 
-    /**
-     * Construct an Expense, giving them the given name,
-     * priority, start date, and end date.
-    *
-    * @param name      The Expense's name
-    * @param value     The Expense's expenditures
-     */
+  /**
+   * Construct an Expense, giving them the given name,
+   * priority, start date, and end date.
+   *
+   * @param name      The Expense's name
+   * @param value     The Expense's expenditures
+   */
    public Expenses(String name, double value) {
        this.name = name;
        this.value = value;
@@ -20,15 +20,15 @@ public class Expenses {
    }
 
    /**
-   * getter method for name
-     * @return a String that describes the Expense's name
-     */
-    public String getName() {return name;}
+    * Getter method for name
+    * @return a String that describes the Expense's name
+    */
+   public String getName() {return name;}
 
     /**
-    * getter method for value
-    * @return a double that describes the Expense value
-    */
+     * getter method for value
+     * @return a double that describes the Expense value
+     */
     public double getValue() {
       return value;
     }
