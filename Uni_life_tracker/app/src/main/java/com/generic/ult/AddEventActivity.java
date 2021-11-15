cede73b6ebcd5a254b7d77872a9f7b16cc9f5eb4
hellokathylee <kathyam.lee@mail.utoronto.ca>
2021-11-14 22:31:27 -0500

    - select end date - event type buttons

--- Uni_life_tracker/app/src/main/java/com/generic/ult/AddEventActivity.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/AddEventActivity.java
@@ -12,24 +12,29 @@ import java.util.Calendar;
 import java.util.Locale;
 
 public class AddEventActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
-    TextView tvStartDate, tvStartTime;
+    TextView tvStartDate, tvStartTime, tvEndDate, tvEndTime, tvAssessment, tvDeadline, tvClassTime, tvStudySession;
     RadioGroup radioGroup;
-//    RadioGroup radioButton;
     ImageView ivBack, ivSave;
 
     int priority, hour, minute;
-//    DatePickerDialog.OnDateSetListener setListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
 
-        tvStartDate = findViewById(R.id.tv_start_date);
-        tvStartTime = findViewById(R.id.tv_start_time);
-        radioGroup = findViewById(R.id.rg_priorities);
         ivBack = findViewById(R.id.iv_back);
         ivSave = findViewById(R.id.iv_save);
+        tvStartDate = findViewById(R.id.tv_start_date);
+        tvStartTime = findViewById(R.id.tv_start_time);
+        tvEndDate = findViewById(R.id.tv_end_date);
+        tvEndTime = findViewById(R.id.tv_end_time);
+        radioGroup = findViewById(R.id.rg_priorities);
+        tvAssessment = findViewById(R.id.tv_assessment);
+        tvDeadline = findViewById(R.id.tv_deadline);
+        tvClassTime = findViewById(R.id.tv_class_time);
+        tvStudySession = findViewById(R.id.tv_study_session);
+
         Spinner coursesList = findViewById(R.id.spn_courses);
 
         Calendar calendar = Calendar.getInstance();
@@ -39,6 +44,10 @@ public class AddEventActivity extends AppCompatActivity implements RadioGroup.On
 
         radioGroup.setOnCheckedChangeListener(this);
         ivBack.setOnClickListener(this::ClickBack);
+        tvAssessment.setOnClickListener(this::ClickAssessment);
+        tvDeadline.setOnClickListener(this::ClickDeadline);
+        tvClassTime.setOnClickListener(this::ClickClassTime);
+        tvStudySession.setOnClickListener(this::ClickStudySession);
 
 //        Start Date Selection
         tvStartDate.setOnClickListener(v -> {
@@ -51,6 +60,17 @@ public class AddEventActivity extends AppCompatActivity implements RadioGroup.On
             datePickerDialog.show();
         });
 
+//        End Date Selection
+        tvEndDate.setOnClickListener(v -> {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    AddEventActivity.this, (view, year1, month1, dayOfMonth) -> {
+                month1 = month1 + 1;
+                String date = dayOfMonth + "/" + month1 + "/" + year1;
+                tvEndDate.setText(date);
+            }, year, month, day);
+            datePickerDialog.show();
+        });
+
 //        Start Time Selection
         tvStartTime.setOnClickListener(v -> {
             TimePickerDialog timePickerDialog = new TimePickerDialog(
@@ -59,6 +79,17 @@ public class AddEventActivity extends AppCompatActivity implements RadioGroup.On
                         minute = selectedMin;
                         tvStartTime.setText(String.format(Locale.getDefault(),"%02d:%02d", hour, minute));
                     }, hour, minute, true);
+            timePickerDialog.show();
+        });
+
+//        Start Time Selection
+        tvEndTime.setOnClickListener(v -> {
+            TimePickerDialog timePickerDialog = new TimePickerDialog(
+                    AddEventActivity.this, (view, selectedHour, selectedMin) -> {
+                hour = selectedHour;
+                minute = selectedMin;
+                tvEndTime.setText(String.format(Locale.getDefault(),"%02d:%02d", hour, minute));
+            }, hour, minute, true);
             timePickerDialog.show();
         });
 
@@ -80,16 +111,36 @@ public class AddEventActivity extends AppCompatActivity implements RadioGroup.On
 //    Priority Selection
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
-        if (checkedId == R.id.high_priority) {
+        if (checkedId == R.id.rb_high_priority) {
             priority = 1;
-        } else if (checkedId == R.id.med_priority) {
+        } else if (checkedId == R.id.rb_med_priority) {
             priority = 2;
-        } else if (checkedId == R.id.low_priority) {
+        } else if (checkedId == R.id.rb_low_priority) {
             priority = 3;
         }
     }
 
     //    Save Button
     public void ClickSave(View view) {
+    }
+
+    public void ClickAssessment(View view) {
+        Intent intent = new Intent(this, MainPageActivity.class); // TODO: direct to assessment page
+        startActivity(intent);
+    }
+
+    public void ClickDeadline(View view) {
+        Intent intent = new Intent(this, MainPageActivity.class); // TODO: direct to deadline page
+        startActivity(intent);
+    }
+
+    public void ClickClassTime(View view) {
+        Intent intent = new Intent(this, MainPageActivity.class); // TODO: direct to class page
+        startActivity(intent);
+    }
+
+    public void ClickStudySession(View view) {
+        Intent intent = new Intent(this, MainPageActivity.class); // TODO: direct to study session page
+        startActivity(intent);
     }
 }
