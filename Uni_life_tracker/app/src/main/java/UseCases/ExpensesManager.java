1a5424112d1ff3174ea254bbd02670c697d80e0b
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-11-15 16:43:33 -0500

    implemented setter method for names in Expenses UseCase

--- Uni_life_tracker/app/src/main/java/UseCases/ExpensesManager.java
+++ Uni_life_tracker/app/src/main/java/UseCases/ExpensesManager.java
@@ -9,4 +9,13 @@ public class ExpensesManager {
      */
     public ExpensesManager() {}
 
+    /**
+     * Changes the name of the Expense
+     * @param e Expenses object
+     * @param name new name for the Expenses object
+     */
+    public void changeName(Expenses e, String name) {
+        e.setName(name);
+    }
+
 }
