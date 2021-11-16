4d130b8c6b11f20ca4c518057db003f34a53afe5
EvgeniaSila <evgenia.silajev@mail.utoronto.ca>
2021-11-15 23:32:51 -0500

    fixing recycler view

--- Uni_life_tracker/app/src/main/java/com/generic/ult/ExpensesRecyclerView.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/ExpensesRecyclerView.java
@@ -7,48 +7,47 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
 
-public class ExpensesRecyclerView extends RecyclerView.Adapter<ExpensesRecyclerView.MyViewHolder> {
-        private ArrayList<Expenses> expensesArrayList;
+public class ExpensesRecyclerView extends RecyclerView.Adapter<ExpensesRecyclerView.MyViewHolder>{
+    private ArrayList<Expenses> expensesArrayList;
 
-        public ExpensesRecyclerView (ArrayList<Expenses> expensesArrayList){
-            this.expensesArrayList = expensesArrayList;
-        }
+    public  ExpensesRecyclerView (ArrayList<Expenses> expensesArrayList){
+        this.expensesArrayList = expensesArrayList;
+    }
 
-        public class MyViewHolder extends RecyclerView.ViewHolder{
-            private TextView expenseTxt, amountTxt, addexpenseTxt,  addamountTxt;
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+       private TextView expenseNameTxt, expenseAmounttimeTxt;
+        //private TextInputLayout expenseNameTxt, expenseAmounttimeTxt;
 
-            public MyViewHolder(final View view){
-                super(view);
-                expenseTxt = view.findViewById(R.id.expense_name);
-                amountTxt = view.findViewById(R.id.expense_amount);
-                addexpenseTxt = view.findViewById(R.id.add_expense_name);
-                addamountTxt = view.findViewById(R.id.add_expense_amount);
-            }
-        }
 
-        @NonNull
-        @Override
-        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_expenses_list, parent, false);
-            return new MyViewHolder(itemView);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-            String expenseName = expensesArrayList.get(position).getName();
-            String amount = "$" + expensesArrayList.get(position).getValue();
-            holder.expenseTxt.setText(expenseName);
-            holder.amountTxt.setText(amount);
-            holder.addexpenseTxt.setText(expenseName);
-            holder.addamountTxt.setText(amount);
-        }
-
-        @Override
-        public int getItemCount() {
-            return expensesArrayList.size();
+        public MyViewHolder(final View view){
+            super(view);
+            expenseNameTxt = view.findViewById(R.id.add_e_name);
+            expenseAmounttimeTxt = view.findViewById(R.id.add_e_amount);
         }
     }
 
+    @NonNull
+    @Override
+    public ExpensesRecyclerView.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_add_expenses, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExpensesRecyclerView.MyViewHolder holder, int position) {
+        String expenseName = expensesArrayList.get(position).getName();
+        //Double expenseAmount = expensesArrayList.get(position).getValue();
+        holder.expenseNameTxt.setText(expenseName);
+        //holder.expenseAmounttimeTxt.setText(expenseAmount);
+    }
+
+    @Override
+    public int getItemCount() {
+        return expensesArrayList.size();
+    }
+}
