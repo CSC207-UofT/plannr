2e86144b44e9b6f7c86bd1f14f50ca943985cfc7
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-11-15 20:29:42 -0500

    corrected a javadoc logical error that caused the tests to fail

--- Uni_life_tracker/app/src/test/java/com/generic/ult/UseCaseTests/EventPriorityComparatorTest.java
+++ Uni_life_tracker/app/src/test/java/com/generic/ult/UseCaseTests/EventPriorityComparatorTest.java
@@ -24,14 +24,14 @@ public class EventPriorityComparatorTest {
     public void TestHigherVLower() {
         Event e1 = new Event("event 1", 1, DATE);
         Event e2 = new Event("event 1", 2, DATE);
-        assertEquals(1, EPC.compare(e1, e2));
+        assertEquals(-1, EPC.compare(e1, e2));
     }
 
     @Test(timeout = 50)
     public void TestLowerVHigher() {
         Event e1 = new Event("event 1", 1, DATE);
         Event e2 = new Event("event 1", 2, DATE);
-        assertEquals(-1, EPC.compare(e2, e1));
+        assertEquals(1, EPC.compare(e2, e1));
     }
 
     @Test(timeout = 50)
