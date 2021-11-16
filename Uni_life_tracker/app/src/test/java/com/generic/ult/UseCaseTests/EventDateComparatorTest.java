369df3b2702b1821c320008bfbfc4d95203eb998
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-11-15 21:14:12 -0500

    setup the test

--- Uni_life_tracker/app/src/test/java/com/generic/ult/UseCaseTests/EventDateComparatorTest.java
+++ Uni_life_tracker/app/src/test/java/com/generic/ult/UseCaseTests/EventDateComparatorTest.java
@@ -1,4 +1,38 @@
 package com.generic.ult.UseCaseTests;
 
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDateTime;
+
+import Entities.Event;
+import UseCases.EventDateComparator;
+
 public class EventDateComparatorTest {
+    EventDateComparator EDC;
+    Event e1;
+    Event e2;
+    Event e3;
+
+    @Before
+    public void setUp() {
+        EDC = new EventDateComparator();
+        e1 = new Event(
+                "test",
+                1,
+                LocalDateTime.of(2021, 1, 1, 1, 1, 1, 1)
+        );
+        e2 = new Event(
+                "test",
+                1,
+                LocalDateTime.of(2021, 1, 2, 1, 1, 1, 1)
+        );
+        e1 = new Event(
+                "test",
+                1,
+                LocalDateTime.of(2021, 1, 2, 1, 1, 1, 1)
+        );
+    }
+
 }
