4c74f2019ac7adea428a804c95c19b55aa571217
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-11-15 21:20:08 -0500

    implemented tests for EventDateComparatorTest

--- Uni_life_tracker/app/src/test/java/com/generic/ult/UseCaseTests/EventDateComparatorTest.java
+++ Uni_life_tracker/app/src/test/java/com/generic/ult/UseCaseTests/EventDateComparatorTest.java
@@ -21,18 +21,36 @@ public class EventDateComparatorTest {
         e1 = new Event(
                 "test",
                 1,
+                LocalDateTime.of(2021, 1, 1, 1, 1, 1, 1),
                 LocalDateTime.of(2021, 1, 1, 1, 1, 1, 1)
         );
         e2 = new Event(
                 "test",
                 1,
+                LocalDateTime.of(2021, 1, 2, 1, 1, 1, 1),
                 LocalDateTime.of(2021, 1, 2, 1, 1, 1, 1)
         );
-        e1 = new Event(
+        e3 = new Event(
                 "test",
                 1,
+                LocalDateTime.of(2021, 1, 2, 1, 1, 1, 1),
                 LocalDateTime.of(2021, 1, 2, 1, 1, 1, 1)
         );
+    }
+
+    @Test(timeout = 50)
+    public void TestSoonerVLater() {
+        assertEquals(-1, EDC.compare(e1, e2));
+    }
+
+    @Test(timeout = 50)
+    public void TestLaterVSooner() {
+        assertEquals(1, EDC.compare(e2, e1));
+    }
+
+    @Test(timeout = 50)
+    public void TestSameDate() {
+        assertEquals(0, EDC.compare(e2, e3));
     }
 
 }
