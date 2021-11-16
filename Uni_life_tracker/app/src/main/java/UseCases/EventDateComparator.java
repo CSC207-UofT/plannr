4b939d44d851f0da984096595420d3bfdcc5cc39
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-11-15 21:23:51 -0500

    fixed typo

--- Uni_life_tracker/app/src/main/java/UseCases/EventDateComparator.java
+++ Uni_life_tracker/app/src/main/java/UseCases/EventDateComparator.java
@@ -15,7 +15,7 @@ public class EventDateComparator implements Comparator<Event> {
      * @param e1 represents an Event
      * @param e2 represents an other Event
      * @return -1 if e1 occurs after e2, 0 if e1 and e2 occur at the same date-time,
-     * and 1 if e1 before after e2.
+     * and 1 if e1 before e2.
      */
     public int compare(@NonNull Event e1, @NonNull Event e2) {
         return e1.getStartDate().compareTo(e2.getStartDate());
