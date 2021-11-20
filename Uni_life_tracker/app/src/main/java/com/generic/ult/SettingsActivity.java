2fb274dff967ca7223adc7859d8d78bcd7e615a4
hellokathylee <kathyam.lee@mail.utoronto.ca>
2021-11-20 00:41:01 -0500

    - add nav menu - rearrange name and university input

--- Uni_life_tracker/app/src/main/java/com/generic/ult/SettingsActivity.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/SettingsActivity.java
@@ -2,23 +2,50 @@ package com.generic.ult;
 
 import android.content.Intent;
 import android.view.View;
-import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class SettingsActivity extends AppCompatActivity {
+    // initialize variable
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        ImageView backBtn = findViewById(R.id.back);
-        backBtn.setOnClickListener(this::ClickBack);
+        // side menu
+        drawerLayout = findViewById(R.id.drawer_layout);
     }
 
     public void ClickBack(View view) {
         Intent intent = new Intent(this, MainPageActivity.class);
         startActivity(intent);
+    }
+
+    public void ClickMenu(View view){
+        // open drawer
+        openDrawer(drawerLayout);
+    }
+
+    private static void openDrawer(DrawerLayout drawerLayout) {
+        // open drawer layout
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void ClickLogo(View view) {
+        // close drawer
+        closeDrawer(drawerLayout);
+    }
+
+    private static void closeDrawer(DrawerLayout drawerLayout) {
+        // close drawer layout
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            // when drawer is open, close drawer
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
     }
 }
