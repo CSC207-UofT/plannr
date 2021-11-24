a17bb8ce32cfb043f983190a4d4a6818424a562a
hellokathylee <kathyam.lee@mail.utoronto.ca>
2021-11-24 01:59:56 -0500

    *error with Handler* - change WelcomeActivity to splash page - omit previous formatting in activity_expenses.xml - documentation in activity_signup.xml

--- Uni_life_tracker/app/src/main/java/com/generic/ult/WelcomeActivity.java
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/WelcomeActivity.java
@@ -1,24 +1,22 @@
 package com.generic.ult;
 
 import android.content.Intent;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
-        Button btnStart = findViewById(R.id.btn_start);
-        btnStart.setOnClickListener(v -> openSignup());
+        
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(WelcomeActivity.this, SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2500);
     }
-
-    //https://www.youtube.com/watch?v=bgIUdb-7Rqo
-   private void openSignup() {
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
-   }
 }
