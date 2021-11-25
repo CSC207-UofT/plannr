62677169c1537c9da0ab7e99e4ba24f8b4cf0948
sarihammad <sari_hammad@hotmail.com>
2021-11-25 10:44:18 -0500

    fixed a small bug and added GetTodaysEvents

--- Uni_life_tracker/app/src/main/java/Database/EventDatabaseHelper.java
+++ Uni_life_tracker/app/src/main/java/Database/EventDatabaseHelper.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -13,7 +14,7 @@ import java.util.List;
 
 import Entities.Event;
 
-public class DatabaseHelper extends SQLiteOpenHelper {
+public class EventDatabaseHelper extends SQLiteOpenHelper {
 
 
     private static final String createEventTable = "CREATE TABLE events(NAME TEXT, PRIORITY " +
@@ -22,7 +23,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final DateTimeFormatter DATEFORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
-    public DatabaseHelper(Context context) {
+    public EventDatabaseHelper(Context context) {
         super(context, "eventDatabase", null, 1);
     }
 
@@ -88,18 +89,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         try {
             cur = db.query("events", null, null, null,
                     null, null, null, null);
-            if (cur != null && cur.moveToFirst()) {
-                do {
-                    LocalDateTime start = LocalDateTime.parse(cur.getString(cur.getColumnIndexOrThrow("START_DATE")), DATEFORMAT);
-                    LocalDateTime end = LocalDateTime.parse(cur.getString(cur.getColumnIndexOrThrow("END_DATE")), DATEFORMAT);
-                    Event event = new Event(cur.getString(cur.getColumnIndexOrThrow("NAME")),
-                                            cur.getInt(cur.getColumnIndexOrThrow("PRIORITY")),
-                                            start,
-                                            end);
-                    eventList.add(event);
+            if (cur != null) {
+                if (cur.moveToFirst()) {
+                    do {
+                        LocalDateTime start = LocalDateTime.parse(cur.getString(cur.getColumnIndexOrThrow("START_DATE")), DATEFORMAT);
+                        LocalDateTime end = LocalDateTime.parse(cur.getString(cur.getColumnIndexOrThrow("END_DATE")), DATEFORMAT);
+                        Event event = new Event(cur.getString(cur.getColumnIndexOrThrow("NAME")),
+                                                cur.getInt(cur.getColumnIndexOrThrow("PRIORITY")),
+                                                start,
+                                                end);
+                        eventList.add(event);
 
-                } while (cur.moveToNext());
+                    } while (cur.moveToNext());
 
+                }
             }
         } finally {
             db.endTransaction();
@@ -109,4 +112,44 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         return eventList;
     }
+
+    /**
+     * Return a list of all events which start today currently in the database
+     *
+     * @return returns the list of Event objects which start today
+     */
+    public List<Event> getTodaysEvents(){
+        List<Event> eventList = new ArrayList<>();
+        Cursor cur = null;
+        db.beginTransaction();
+        try {
+            cur = db.query("events", null, null, null,
+                    null, null, null, null);
+            if (cur != null) {
+                if (cur.moveToFirst()) {
+                    do {
+                        LocalDateTime start = LocalDateTime.parse(cur.getString(cur.getColumnIndexOrThrow("START_DATE")), DATEFORMAT);
+                        if (!(start.toLocalDate().isEqual(LocalDate.now()))) {
+                            continue;
+                        }
+                        LocalDateTime end = LocalDateTime.parse(cur.getString(cur.getColumnIndexOrThrow("END_DATE")), DATEFORMAT);
+                        Event event = new Event(cur.getString(cur.getColumnIndexOrThrow("NAME")),
+                                cur.getInt(cur.getColumnIndexOrThrow("PRIORITY")),
+                                start,
+                                end);
+                        eventList.add(event);
+
+                    } while (cur.moveToNext());
+
+                }
+            }
+        } finally {
+            db.endTransaction();
+            assert cur != null;
+            cur.close();
+        }
+
+        return eventList;
+    }
+
 }
