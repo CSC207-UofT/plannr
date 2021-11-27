e8d49fab55b986acf87cd4b392d90fd1d0a72740
sarihammad <sari_hammad@hotmail.com>
2021-11-27 12:39:12 -0500

    removed getTodaysEvents

--- Uni_life_tracker/app/src/main/java/Database/EventDatabaseHelper.java
+++ Uni_life_tracker/app/src/main/java/Database/EventDatabaseHelper.java
@@ -108,45 +108,6 @@ public class EventDatabaseHelper extends SQLiteOpenHelper {
         return eventList;
     }
 
-    /**
-     * Return a list of all events which start today currently in the database
-     *
-     * @return returns the list of Event objects which start today
-     */
-    public List<Event> getTodaysEvents(){
-        List<Event> eventList = new ArrayList<>();
-        Cursor cur = null;
-        db.beginTransaction();
-        try {
-            cur = db.query("events", null, null, null,
-                    null, null, null, null);
-            if (cur != null) {
-                if (cur.moveToFirst()) {
-                    do {
-                        LocalDateTime start = LocalDateTime.parse(cur.getString(cur.getColumnIndexOrThrow("START_DATE")), DATEFORMAT);
-                        if (!(start.toLocalDate().isEqual(LocalDate.now()))) {
-                            continue;
-                        }
-                        LocalDateTime end = LocalDateTime.parse(cur.getString(cur.getColumnIndexOrThrow("END_DATE")), DATEFORMAT);
-                        Event event = new Event(cur.getString(cur.getColumnIndexOrThrow("NAME")),
-                                cur.getInt(cur.getColumnIndexOrThrow("PRIORITY")),
-                                start,
-                                end);
-                        eventList.add(event);
-
-                    } while (cur.moveToNext());
-
-                }
-            }
-        } finally {
-            db.endTransaction();
-            assert cur != null;
-            cur.close();
-        }
-
-        return eventList;
-    }
-
 
     /**
      * Return a list of all events which start at date currently in the database
