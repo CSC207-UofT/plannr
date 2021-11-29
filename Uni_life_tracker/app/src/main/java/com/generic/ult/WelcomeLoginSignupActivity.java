e5e72432e803ad17bd5cd5e436912906004c105e
EvgeniaSila <evgenia.silajev@mail.utoronto.ca>
2021-11-29 15:37:18 -0500

    feat: created new welcome page activity

---
+++ Uni_life_tracker/app/src/main/java/com/generic/ult/WelcomeLoginSignupActivity.java
@@ -0,0 +1,34 @@
+package com.generic.ult;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class WelcomeLoginSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_login_signup);
+        Button btnSignUp = findViewById(R.id.btn_sign_up_main);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeLoginSignupActivity.this, SignUpActivity.class));
+            }
+        });
+
+
+    }
+}
+
+
+
+
+
+
+
