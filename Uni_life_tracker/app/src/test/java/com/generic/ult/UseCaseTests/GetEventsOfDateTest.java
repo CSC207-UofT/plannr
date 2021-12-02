153f2c642ed0b612a047f03ea7b963b88ebbe518
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-01 22:18:25 -0500

    test: implemented basic test for GetEventsOfDate

--- Uni_life_tracker/app/src/test/java/com/generic/ult/UseCaseTests/GetEventsOfDateTest.java
+++ Uni_life_tracker/app/src/test/java/com/generic/ult/UseCaseTests/GetEventsOfDateTest.java
@@ -1,4 +1,39 @@
 package com.generic.ult.UseCaseTests;
 
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static UseCases.GetEventsOfDate.getEventsOfDate;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Random;
+
+import Entities.Event;
+
 public class GetEventsOfDateTest {
+    LocalDateTime date_;
+    Random rand;
+    @Before
+    public void setUp() {
+        date_ = LocalDateTime.of(2021, 1, 1, 1, 1, 1, 1);
+        rand = new Random();
+    }
+    /**
+     * This tests the basic functionality of the UseCase GetEventsOfDate
+     */
+    @Test
+    public void TestBasicFunctions() {
+        // Empty arraylist to store events for testing
+        ArrayList<Event> events = new ArrayList<>();
+        for (int i = 0; i < 10;  i++) {
+            events.add(new Event("test", 1, date_, date_));
+        }
+        // Add an event that is 1 day later
+        events.add(new Event("test", 1, date_.plusDays(1), date_));
+
+       // Assert Equals
+        assertEquals(10, getEventsOfDate(events, date_.toLocalDate()).size());
+    }
+
 }
