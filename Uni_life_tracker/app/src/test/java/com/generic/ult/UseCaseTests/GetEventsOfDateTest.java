8b8e28a1c77cdfb216e8879084b27e9b5a76e34b
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-01 22:18:48 -0500

    test: implemented test that randomly generates date and number of dates

--- Uni_life_tracker/app/src/test/java/com/generic/ult/UseCaseTests/GetEventsOfDateTest.java
+++ Uni_life_tracker/app/src/test/java/com/generic/ult/UseCaseTests/GetEventsOfDateTest.java
@@ -36,4 +36,41 @@ public class GetEventsOfDateTest {
         assertEquals(10, getEventsOfDate(events, date_.toLocalDate()).size());
     }
 
+    /**
+     * This test will randomly generate numbers and do assert equals on every iteration
+     */
+    @Test(timeout = 10000)
+    public void TestRandom() {
+        // Randomly defines how many times the loop runs
+        int iter = rand.nextInt(40) + 20;
+
+        while (iter > 0) {
+            // The target date
+            LocalDateTime target = date_.plusDays(rand.nextInt(3));
+            // Counter to count how many times we added events of the same date
+            int counter = 0;
+            // Creates a list to store the events
+            ArrayList<Event> events = new ArrayList<>();
+
+            // Add events to the list
+            for (int i = 0; i < 15; i++) {
+                // To store the randomly generated date
+                LocalDateTime startDate = date_.plusDays(rand.nextInt(3));
+                if (startDate.compareTo(target) == 0) {
+                    counter += 1;
+                }
+                events.add(new Event(
+                        "test",
+                        1,
+                        startDate,
+                        date_.plusDays(rand.nextInt(100))
+                ));
+            }
+
+            // Compare the size of the return and
+            assertEquals(counter, getEventsOfDate(events, target.toLocalDate()).size());
+
+            iter -= 1;
+        }
+    }
 }
