9e68e6b8020fd35a9b29ba3c5a70cad96ae3eced
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-02 16:26:06 -0500

    test: implemented test to test viewEventList when there is zero events in user's eventList

--- Uni_life_tracker/app/src/test/java/com/generic/plannr/UseCaseTests/UserManagerTest.java
+++ Uni_life_tracker/app/src/test/java/com/generic/plannr/UseCaseTests/UserManagerTest.java
@@ -1,4 +1,25 @@
 package com.generic.plannr.UseCaseTests;
 
+import static junit.framework.TestCase.assertEquals;
+import static com.generic.plannr.StaticTestVariables.*;
+
+import com.generic.plannr.Entities.Event;
+import com.generic.plannr.UseCases.UserManager;
+
+import org.junit.*;
+
+import java.util.ArrayList;
+
 public class UserManagerTest {
+    UserManager user;
+
+    @Before
+    public void setUp() {
+        user = new UserManager("Test User", COURSES, "U of T");
+    }
+
+    @Test
+    public void TestViewEventListEmpty() {
+        assertEquals(new ArrayList<>(), user.viewEventList());
+    }
 }
