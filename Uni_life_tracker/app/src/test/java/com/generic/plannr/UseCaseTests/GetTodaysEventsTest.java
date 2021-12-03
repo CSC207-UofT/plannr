436a5d7a02d46c162708ca720e10cce0062ef570
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-02 23:35:13 -0500

    test: implemented basic tests for GetTodaysEvents

--- Uni_life_tracker/app/src/test/java/com/generic/plannr/UseCaseTests/GetTodaysEventsTest.java
+++ Uni_life_tracker/app/src/test/java/com/generic/plannr/UseCaseTests/GetTodaysEventsTest.java
@@ -11,6 +11,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class GetTodaysEventsTest {
     ArrayList<Event> events;
@@ -25,5 +27,34 @@ public class GetTodaysEventsTest {
     @Test
     public void TestEmptyEventList() {
         assertEquals(new ArrayList<>(), getTodaysEvents(user));
+    }
+
+    /**
+     * This tests the method given a user that only has one event and it also happens today
+     */
+    @Test
+    public void Test1EventOfToday() {
+        Event eventToday = new Event("test", 0, TODAY, TODAY);
+        user.addEventToUsersList(eventToday);
+        ArrayList<Event> expected = new ArrayList<>(Collections.singletonList(eventToday));
+
+        assertEquals(1, getTodaysEvents(user).size());
+        assertEquals(expected, getTodaysEvents(user));
+    }
+
+    /**
+     * This tests the method given a user that only has 2 events and they also happen today
+     */
+    @Test
+    public void Test2EventOfToday() {
+        Event eventToday1 = new Event("test", 0, TODAY, TODAY);
+        Event eventToday2 = new Event("test", 0, TODAY, TODAY);
+
+        user.addEventToUsersList(eventToday1);
+        user.addEventToUsersList(eventToday2);
+        ArrayList<Event> expected = new ArrayList<>(Arrays.asList(eventToday1, eventToday2));
+
+        assertEquals(2, getTodaysEvents(user).size());
+        assertEquals(expected, getTodaysEvents(user));
     }
 }
