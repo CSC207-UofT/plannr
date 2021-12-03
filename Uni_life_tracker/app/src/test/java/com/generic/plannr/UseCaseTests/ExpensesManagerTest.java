2ba3d8ed86058868a6f25e87ab702cd44f0f5740
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-02 23:03:33 -0500

    test: implemented test for createExpense

--- Uni_life_tracker/app/src/test/java/com/generic/plannr/UseCaseTests/ExpensesManagerTest.java
+++ Uni_life_tracker/app/src/test/java/com/generic/plannr/UseCaseTests/ExpensesManagerTest.java
@@ -1,4 +1,24 @@
 package com.generic.plannr.UseCaseTests;
 
+import com.generic.plannr.Entities.Expense;
+import com.generic.plannr.UseCases.ExpensesManager;
+
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+
 public class ExpensesManagerTest {
+    ExpensesManager expManager;
+
+    @Before
+    public void setUp() {
+        expManager = new ExpensesManager();
+    }
+
+    @Test
+    public void TestCreateExpense() {
+        Expense exp = expManager.createExpense("test", 100.0);
+        assertEquals("test", expManager.getExpenseName(exp));
+        assertEquals(100, expManager.getValue(exp), 0.0);
+    }
 }
