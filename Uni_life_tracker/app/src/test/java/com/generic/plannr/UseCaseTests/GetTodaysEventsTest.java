1b0c0394b5e91cd0617fd28346d366b6a73b0266
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-02 23:19:04 -0500

    test: implemented test for newly created users (empty eventList)

--- Uni_life_tracker/app/src/test/java/com/generic/plannr/UseCaseTests/GetTodaysEventsTest.java
+++ Uni_life_tracker/app/src/test/java/com/generic/plannr/UseCaseTests/GetTodaysEventsTest.java
@@ -1,4 +1,29 @@
 package com.generic.plannr.UseCaseTests;
 
+import static org.junit.Assert.assertEquals;
+import static com.generic.plannr.UseCases.GetTodaysEvents.getTodaysEvents;
+import static com.generic.plannr.StaticTestVariables.*;
+
+import com.generic.plannr.Entities.Event;
+import com.generic.plannr.UseCases.UserManager;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class GetTodaysEventsTest {
+    ArrayList<Event> events;
+    UserManager user;
+
+    @Before
+    public void setUp() {
+        events = new ArrayList<>();
+        user = new UserManager("test user", COURSES, "U of Tears and Tuition");
+    }
+
+    @Test
+    public void TestEmptyEventList() {
+        assertEquals(new ArrayList<>(), getTodaysEvents(user));
+    }
 }
