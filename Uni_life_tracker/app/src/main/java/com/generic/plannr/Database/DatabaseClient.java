9c254353c373e58ab776a2ce921085968a5dfd23
sarihammad <sari_hammad@hotmail.com>
2021-12-04 16:35:40 -0500

    fix: added USER_ID column to events and expenses tables

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/Database/DatabaseClient.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/Database/DatabaseClient.java
@@ -14,10 +14,10 @@ public class DatabaseClient extends SQLiteOpenHelper {
 
     private static final String createEventTable = "CREATE TABLE events(ID INTEGER PRIMARY KEY " +
             "AUTOINCREMENT, NAME TEXT, PRIORITY INTEGER, START_DATE TEXT, END_DATE TEXT, " +
-            "EMAIL TEXT)";
+            "EMAIL TEXT, USER_ID INTEGER)";
 
     private static final String createExpenseTable = "CREATE TABLE expenses(ID INTEGER PRIMARY KEY " +
-            "AUTOINCREMENT, NAME TEXT, VALUE DOUBLE, EMAIL TEXT)";
+            "AUTOINCREMENT, NAME TEXT, VALUE DOUBLE, EMAIL TEXT, USER_ID INTEGER)";
 
     public SQLiteDatabase db;
 
