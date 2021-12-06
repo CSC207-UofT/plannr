1d867c2343cf7a60e2d0284d58fa15d227ba6f84
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-06 00:55:44 -0500

    test: created EventGatewayTest

---
+++ Uni_life_tracker/app/src/androidTest/java/com/generic/plannr/gateway_tests/EventGatewayTest.java
@@ -0,0 +1,33 @@
+package com.generic.plannr.gateway_tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EventGatewayTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void openDatabase() {
+    }
+
+    @Test
+    public void saveToDatabase() {
+    }
+
+    @Test
+    public void getByID() {
+    }
+
+    @Test
+    public void getAllEvents() {
+    }
+
+    @Test
+    public void getEventsByDate() {
+    }
+}
