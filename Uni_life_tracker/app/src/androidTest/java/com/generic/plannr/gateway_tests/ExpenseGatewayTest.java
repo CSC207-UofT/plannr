7cdfdcaf2cb23060c8c0dc85972ace341d1f595d
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-06 17:22:23 -0500

    test: implemented test for openDatabase

---
+++ Uni_life_tracker/app/src/androidTest/java/com/generic/plannr/gateway_tests/ExpenseGatewayTest.java
@@ -0,0 +1,53 @@
+package com.generic.plannr.gateway_tests;
+
+import static org.junit.Assert.assertEquals;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.generic.plannr.Entities.Expense;
+import com.generic.plannr.Entities.User;
+import com.generic.plannr.Gateways.ExpenseGateway;
+import com.generic.plannr.Gateways.UserGateway;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExpenseGatewayTest {
+    Context context;
+    ExpenseGateway eg;
+    UserGateway ug;
+
+    @Before
+    public void setUp() {
+        context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        ug = new UserGateway(context);
+        eg = new ExpenseGateway(context);
+        ug.openDatabase();
+        eg.openDatabase();
+        // Saves the user to database
+        ug.saveToDatabase(
+                new User("test", "test@gmail.com", "Test@123")
+        );
+    }
+
+    @After
+    public void finishUp() {
+        context.deleteDatabase("PlannrDatabase");
+    }
+
+    @Test
+    public void openDatabase() {
+        assertEquals(0, eg.getAllExpenses(ug.getLoggedInUserID()).size());
+    }
+
+    @Test
+    public void saveToDatabase() {
+    }
+
+    @Test
+    public void getByID() {
+    }
+}
