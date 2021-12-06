a19527ff1f97a54ff251602f97bc4571669f5248
sarihammad <sari_hammad@hotmail.com>
2021-12-05 21:23:11 -0500

    feat: implemented EmailValidator for validating emails

---
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/EmailValidator.java
@@ -0,0 +1,36 @@
+package com.generic.plannr;
+
+import android.util.Patterns;
+
+import com.generic.plannr.Gateways.UserGateway;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
+
+public class EmailValidator implements Validator {
+
+
+    public boolean validateEntry(TextInputLayout userInput, UserGateway ug, TextInputLayout tiEmail,
+                                 TextInputLayout tiPassword, boolean isSignup) {
+        String input = Objects.requireNonNull(userInput.getEditText()).getText().toString().trim();
+        String email = Objects.requireNonNull(tiEmail.getEditText()).getText().toString();
+
+        if (input.isEmpty()) {
+            userInput.setError("Field cannot be empty");
+            return false;
+        } else if (isSignup && userInput == tiEmail && !Patterns.EMAIL_ADDRESS.matcher(input).matches()) {
+            userInput.setError("Please enter a valid email address");
+            return false;
+        } else if(isSignup && userInput == tiEmail && !ug.uniqueEmail(input)){
+            userInput.setError("This email is already being used");return false;
+
+        } else if (!isSignup && userInput == tiEmail && ug.uniqueEmail(email)) {
+            userInput.setError("The email you entered does not belong to any account");
+            return false;
+        } else {
+            userInput.setError(null);
+            ug.updateLoggedInUser(email);
+            return true;
+        }
+    }
+}
