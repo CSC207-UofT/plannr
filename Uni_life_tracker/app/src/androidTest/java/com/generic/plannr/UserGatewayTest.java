163d286a1ed5f5d789326113fb01db7b97d8a12c
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-05 23:28:37 -0500

    test: created a package to host Android tests and created UserGatewayTest

---
+++ Uni_life_tracker/app/src/androidTest/java/com/generic/plannr/UserGatewayTest.java
@@ -0,0 +1,63 @@
+package com.generic.plannr;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserGatewayTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void openDatabase() {
+    }
+
+    @Test
+    public void saveToDatabase() {
+    }
+
+    @Test
+    public void updateName() {
+    }
+
+    @Test
+    public void updatePassword() {
+    }
+
+    @Test
+    public void getByID() {
+    }
+
+    @Test
+    public void getByEmail() {
+    }
+
+    @Test
+    public void getLoggedInUserID() {
+    }
+
+    @Test
+    public void getLoggedInName() {
+    }
+
+    @Test
+    public void getLoggedInEmail() {
+    }
+
+    @Test
+    public void getPassword() {
+    }
+
+    @Test
+    public void updateLoggedInUser() {
+    }
+
+    @Test
+    public void uniqueEmail() {
+    }
+
+    @Test
+    public void getAllUsers() {
+    }
+}
