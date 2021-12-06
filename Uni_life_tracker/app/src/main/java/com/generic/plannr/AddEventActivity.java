7e5256f0c206b566bd97b53a7e686ac2b735ed34
Kathy Lee <kathyam.lee@mail.utoronto.ca>
2021-12-05 21:14:32 -0500

    doc: add javadocs and reorder methods

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/AddEventActivity.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/AddEventActivity.java
@@ -1,3 +1,7 @@
+/* Plannr by Generic Name
+ *
+ * This file contains methods for activity_add_event.xml.
+ */
 package com.generic.plannr;
 
 import android.annotation.SuppressLint;
@@ -12,7 +16,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.generic.plannr.Entities.Event;
 import com.generic.plannr.Gateways.EventGateway;
 import com.generic.plannr.Gateways.UserGateway;
-import android.os.Bundle;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -38,7 +41,7 @@ public class AddEventActivity extends AppCompatActivity implements RadioGroup.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
 
-//        Assign Variables
+        activity = new MainActivity();
         ivBack = findViewById(R.id.iv_back);
         ivSave = findViewById(R.id.iv_save);
         etEventName = findViewById(R.id.et_event_name);
@@ -52,8 +55,6 @@ public class AddEventActivity extends AppCompatActivity implements RadioGroup.On
         tvDeadline = findViewById(R.id.tv_deadline);
         tvClassTime = findViewById(R.id.tv_class_time);
         tvStudySession = findViewById(R.id.tv_study_session);
-
-        activity = new MainActivity();
 
 //       Initialize Calendar
         Calendar calendar = Calendar.getInstance();
@@ -161,14 +162,8 @@ public class AddEventActivity extends AppCompatActivity implements RadioGroup.On
         });
     }
 
-//    Back Button
-    public void clickBack(View view) {
-        activity.redirectActivity(this, SchoolActivity.class);
-    }
-
-//    Priority Selection
     @Override
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
+    public void onCheckedChanged(RadioGroup group, int checkedId) { // Priority selection
         if (checkedId == R.id.rb_high_priority) {
             priority = 1;
         } else if (checkedId == R.id.rb_med_priority) {
@@ -178,54 +173,10 @@ public class AddEventActivity extends AppCompatActivity implements RadioGroup.On
         }
     }
 
-    public void clickAssessment(View view) {
-        activity.redirectActivity(this, MainActivity.class);  // TODO: direct to assessment page
-    }
-
-    public void clickDeadline(View view) {
-        activity.redirectActivity(this, MainActivity.class);  //TODO: direct to deadline page
-    }
-
-    public void clickClassTime(View view) {
-        activity.redirectActivity(this, MainActivity.class);  // TODO: direct to class page
-    }
-
-    public void clickStudySession(View view) {
-        activity.redirectActivity(this, MainActivity.class);  // // TODO: direct to study session page
-    }
-
     /**
-     * Saves the event into the database
-     */
-    public void clickSaveEvent(View view) {
-        if (addEventInput()) {
-
-            String eventName = etEventName.getText().toString();
-            String startDate = tvStartDate.getText().toString().trim();
-            String endDate = tvEndDate.getText().toString().trim();
-            String startTime = tvStartTime.getText().toString().trim();
-            String endTime = tvEndTime.getText().toString().trim();
-
-
-            DateTimeFormatter DATEFORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm a");
-
-            LocalDateTime start = LocalDateTime.parse(startDate + " " + startTime, DATEFORMAT);
-            LocalDateTime end = LocalDateTime.parse(endDate + " " + endTime, DATEFORMAT);
-
-            String email = ug.getLoggedInEmail();
-
-            Event event = new Event(eventName, priority, start, end);
-            int userID = ug.getLoggedInUserID();
-
-            eg.saveToDatabase(event, userID);
-
-            activity.redirectActivity(this, SchoolActivity.class);
-        }
-    }
-    /**
-     * Highlights any textview that is in empty or in an incorrect format
+     * Highlights any TextView text that is in empty or in an incorrect format.
      *
-     * @return whether the added event includes all it's needed attributes
+     * @return  whether the added event includes all its needed attributes
      */
     public boolean addEventInput() {
         Validator input = new Validator();
@@ -233,7 +184,6 @@ public class AddEventActivity extends AppCompatActivity implements RadioGroup.On
         String endDate = tvEndDate.getText().toString().trim();
         String startTime = tvStartTime.getText().toString().trim();
         String endTime = tvEndTime.getText().toString().trim();
-
 
         DateTimeFormatter DATEFORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm a");
 
@@ -256,13 +206,88 @@ public class AddEventActivity extends AppCompatActivity implements RadioGroup.On
         return input.validateAddEvent(etEventName) & input.validateAddEvent(tvEndDate)
                 & input.validateAddEvent(tvEndTime) & input.validateAddEvent(etCourse) & endTimeAfter;
     }
+
     /**
-     * Changes the color of the textview
+     * Change the text color of the TextView.
      *
-     * @param color the r value for the rgb color\
+     * @param color an int representing the R-value for the RGB color.
      */
     public void changeTextColor(int color) {
         tvEndDate.setTextColor(Color.rgb(color, 0, 0));
         tvEndTime.setTextColor(Color.rgb(color, 0, 0));
+    }
+
+    /**
+     * Directs activity to the School activity on back arrow icon click.
+     *
+     * @param view  a View for the device screen.
+     */
+    public void clickBack(View view) {
+        activity.redirectActivity(this, SchoolActivity.class);
+    }
+
+    /**
+     * Saves event into the database.
+     *
+     * @param view  a View for the device screen.
+     */
+    public void clickSaveEvent(View view) {
+        if (addEventInput()) {
+            String eventName = etEventName.getText().toString();
+            String startDate = tvStartDate.getText().toString().trim();
+            String endDate = tvEndDate.getText().toString().trim();
+            String startTime = tvStartTime.getText().toString().trim();
+            String endTime = tvEndTime.getText().toString().trim();
+
+            DateTimeFormatter DATEFORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm a");
+
+            LocalDateTime start = LocalDateTime.parse(startDate + " " + startTime, DATEFORMAT);
+            LocalDateTime end = LocalDateTime.parse(endDate + " " + endTime, DATEFORMAT);
+
+            String email = ug.getLoggedInEmail();
+
+            Event event = new Event(eventName, priority, start, end);
+            int userID = ug.getLoggedInUserID();
+
+            eg.saveToDatabase(event, userID);
+
+            activity.redirectActivity(this, SchoolActivity.class);
+        }
+    }
+
+    /**
+     * Directs activity to the Add Assessment Event activity on assessment icon click.
+     *
+     * @param view  a View for the device screen.
+     */
+    public void clickAssessment(View view) {
+        activity.redirectActivity(this, MainActivity.class);  // TODO: direct to assessment page
+    }
+
+    /**
+     * Directs activity to the Add Deadline Event activity on deadline icon click.
+     *
+     * @param view  a View for the device screen.
+     */
+    public void clickDeadline(View view) {
+        activity.redirectActivity(this, MainActivity.class);  //TODO: direct to deadline page
+    }
+
+    /**
+     * Directs activity to the Add Class Event activity on class icon click.
+     *
+     * @param view  a View for the device screen.
+     */
+    public void clickClassTime(View view) {
+        activity.redirectActivity(this, MainActivity.class);  // TODO: direct to class page
+    }
+
+    /**
+     * Directs activity to the Add Study Session Event activity on study session icon click.
+     *
+     * @param view  a View for the device screen.
+     */
+    public void clickStudySession(View view) {
+        activity.redirectActivity(this, MainActivity.class);  // // TODO: direct to study session page
     }
 }
