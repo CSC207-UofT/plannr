5f663814310facbb69e9102a6954b54ed50b131b
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-07 18:04:00 -0500

    test: updated StaticTestVariables to reflect change to SchoolEvent

--- Uni_life_tracker/app/src/test/java/com/generic/plannr/tests/StaticTestVariables.java
+++ Uni_life_tracker/app/src/test/java/com/generic/plannr/tests/StaticTestVariables.java
@@ -1,6 +1,6 @@
 package com.generic.plannr.tests;
 
-import com.generic.plannr.Entities.Event;
+import com.generic.plannr.Entities.SchoolEvent;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -8,21 +8,21 @@ import java.util.Arrays;
 
 public class StaticTestVariables {
     public static LocalDateTime defaultDate = LocalDateTime.of(2021, 11, 15, 1, 1, 1);
-    public static Event defaultEvent = new Event("Default Event", 0, defaultDate, defaultDate);
+    public static SchoolEvent defaultEvent = new SchoolEvent("deadline", "Default Event", 0, defaultDate, defaultDate, "CSC207");
     public static LocalDateTime TODAY = LocalDateTime.now();
     public static ArrayList<String> COURSES = new ArrayList<>(Arrays.asList("CSC207", "CSC148", "CSC165", "CSC311", "CSC236"));
 
     // This eventList differs only in priorities
-    public static ArrayList<Event> priorityEventList = new ArrayList<>(Arrays.asList(
-            new Event("test", 2, defaultDate, defaultDate),
-            new Event("test", 1, defaultDate, defaultDate),
-            new Event("test", 0, defaultDate, defaultDate)
+    public static ArrayList<SchoolEvent> priorityEventList = new ArrayList<>(Arrays.asList(
+            new SchoolEvent("class", "test", 2, defaultDate, defaultDate, "CSC207", "Bahen"),
+            new SchoolEvent("deadline", "test", 1, defaultDate, defaultDate, "CSC207"),
+            new SchoolEvent("assessment", "test", 0, defaultDate, defaultDate, "CSC207")
     ));
 
     // This eventList differs only in days
-    public static ArrayList<Event> dayEventList = new ArrayList<>(Arrays.asList(
-            new Event("test", 0, defaultDate, defaultDate),
-            new Event("test", 0, defaultDate.plusDays(2), defaultDate.plusDays(2)),
-            new Event("test", 0, defaultDate.plusDays(1), defaultDate.plusDays(1))
+    public static ArrayList<SchoolEvent> dayEventList = new ArrayList<>(Arrays.asList(
+            new SchoolEvent("assessment", "test", 0, defaultDate, defaultDate, "CSC207"),
+            new SchoolEvent("deadline", "test", 0, defaultDate.plusDays(2), defaultDate.plusDays(2), "CSC207"),
+            new SchoolEvent("class", "test", 0, defaultDate.plusDays(1), defaultDate.plusDays(1), "CSC207", "Bahen")
     ));
 }
