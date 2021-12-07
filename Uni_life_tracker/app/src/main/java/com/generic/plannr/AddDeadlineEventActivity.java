c62ffd4d0d3987df6a8ed469c18e94ebe8892973
Kathy Lee <kathyam.lee@mail.utoronto.ca>
2021-12-07 04:35:08 -0500

    !feat: implement deadline event

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/AddDeadlineEventActivity.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/AddDeadlineEventActivity.java
@@ -1,43 +1,61 @@
+/* Plannr by Generic Name
+ *
+ * This file contains methods for activity_add_deadline_event.xml.
+ */
 package com.generic.plannr;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.generic.plannr.AddEventActivity;
+import com.generic.plannr.Entities.Event;
+import com.generic.plannr.Gateways.EventGateway;
+import com.generic.plannr.Gateways.UserGateway;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 public class AddDeadlineEventActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
-    //    Initialize Variables
-    int eventYear, eventMonth, eventDay, eventHour, eventMinute, priority;
+    int yr, mth, day, hr, min, priority;
+    String date, endDate, time, endTime;
     TextView tvDate, tvTime;
-    RadioGroup radioGroup;
+    RadioGroup rgPriorities;
     ImageView ivBack, ivSave;
-    EditText etEventName;
+    EditText etEventName, etCourse;
+    Calendar calendar;
+    DateTimeFormatter DATEFORMAT;
+    LocalDateTime start;
     private MainActivity activity;
+    UserGateway ug = new UserGateway(AddDeadlineEventActivity.this);
+    EventGateway eg = new EventGateway(AddDeadlineEventActivity.this);
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_deadline_event);
 
-//        Assign Variables
+        activity = new MainActivity();
         ivBack = findViewById(R.id.iv_back);
         ivSave = findViewById(R.id.iv_save);
         etEventName = findViewById(R.id.et_event_name);
+        etCourse = findViewById(R.id.et_course);
         tvDate = findViewById(R.id.tv_date);
         tvTime = findViewById(R.id.tv_time);
-        radioGroup = findViewById(R.id.rg_priorities);
+        rgPriorities = findViewById(R.id.rg_priorities);
 
+//        Event navigation bar
         BottomNavigationView navEvents = findViewById(R.id.nav_events);
         navEvents.setSelectedItemId(R.id.nav_deadline);
         navEvents.setOnNavigationItemSelectedListener(item -> {
@@ -62,79 +80,166 @@ public class AddDeadlineEventActivity extends AppCompatActivity implements Radio
             return false;
         });
 
-//       Initialize Calendar
-        Calendar calendar = Calendar.getInstance();
-        eventYear = calendar.get(Calendar.YEAR);
-        eventMonth = calendar.get(Calendar.MONTH);
-        eventDay = calendar.get(Calendar.DAY_OF_MONTH);
-        eventHour = calendar.get(Calendar.HOUR_OF_DAY);
-        eventMinute = calendar.get(Calendar.MINUTE);
+        calendar = Calendar.getInstance();
+        yr = calendar.get(Calendar.YEAR);
+        mth = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        hr = calendar.get(Calendar.HOUR_OF_DAY);
+        min = calendar.get(Calendar.MINUTE);
 
-//      Date & Time Format
+//        Set start date & time to current date & time
         String date = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
         String time = new SimpleDateFormat("hh:mm aa", Locale.getDefault()).format(new Date());
-
         tvDate.setText(date);
         tvTime.setText(time);
 
-        radioGroup.setOnCheckedChangeListener(this);
-        ivBack.setOnClickListener(this::ClickBack);
-
-        ivSave.setOnClickListener(v -> {
-            String eventName = etEventName.getText().toString();
-            Toast.makeText(AddDeadlineEventActivity.this, eventName, Toast.LENGTH_SHORT).show(); // TODO: remove
-        });
-
-//        Date Selection
-        tvDate.setOnClickListener(v -> {
-            DatePickerDialog datePickerDialog = new DatePickerDialog(
-                    AddDeadlineEventActivity.this, (view, year, month, dayOfMonth) -> {
-                eventYear = year;
-                eventMonth = month;
-                eventDay = dayOfMonth;
-
-                String eventDate = eventDay + "-" + (eventMonth+1) + "-" + eventYear;
-                tvDate.setText(eventDate);
-            }, eventYear, eventMonth, eventDay);
-            datePickerDialog.updateDate(eventYear, eventMonth, eventDay); // displays prev selected date
-            datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis()); // disable past date selection
-            datePickerDialog.show();
-        });
-
-//        Time Selection
-        tvTime.setOnClickListener(v -> {
-            TimePickerDialog timePickerDialog = new TimePickerDialog(
-                    AddDeadlineEventActivity.this, (view, hourOfDay, minute) -> {
-                eventHour = hourOfDay;
-                eventMinute = minute;
-
-                Calendar calendar12 = Calendar.getInstance(); // initialize calendar
-                String eventDate = tvDate.getText().toString().trim();
-                String[] strings = eventDate.split("-");
-                int sDay = Integer.parseInt(strings[0]);
-                calendar12.set(Calendar.DAY_OF_MONTH, sDay);
-                calendar12.set(Calendar.HOUR_OF_DAY, eventHour);
-                calendar12.set(Calendar.MINUTE, eventMinute);
-                tvTime.setText(DateFormat.format("hh:mm aa", calendar12));
-            }, eventHour, eventMinute, false);
-            timePickerDialog.show();
-        });
+        rgPriorities.setOnCheckedChangeListener(this);
     }
 
-    //    Back Button
-    public void ClickBack(View view) {
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) { // Priority selection
+        if (checkedId == R.id.rb_high_priority) {
+            priority = 0;
+        } else if (checkedId == R.id.rb_med_priority) {
+            priority = 1;
+        } else if (checkedId == R.id.rb_low_priority) {
+            priority = 2;
+        }
+    }
+
+    /**
+     * Highlights any TextView text that is in empty or in an incorrect format.
+     *
+     * @return whether the added event includes all its needed attributes
+     */
+    public boolean addEventInput() {
+        Validator input = new Validator();
+        setDateTime();
+
+        boolean endTimeAfter = false;
+        if (!endTime.isEmpty()) {
+            LocalDateTime end = LocalDateTime.parse(endDate + " " + endTime, DATEFORMAT);
+            // Checks if end date is after start date with its date time format
+            if (!end.isAfter(start)) {
+                // Warns user that the format is incorrect
+                Toast.makeText(this, "End date should be before start!", Toast.LENGTH_LONG).show();
+                changeTextColor(255);
+            } else {
+                changeTextColor(0);
+                endTimeAfter = true;
+            }
+        }
+
+        return input.validateAddEvent(etEventName) & input.validateAddEvent(tvDate)
+                & input.validateAddEvent(tvTime) & input.validateAddEvent(etCourse) & endTimeAfter;
+    }
+
+    /**
+     * Change the text color of the TextView.
+     *
+     * @param color an int representing the R-value for the RGB color.
+     */
+    public void changeTextColor(int color) {
+        tvDate.setTextColor(Color.rgb(color, 0, 0));
+        tvTime.setTextColor(Color.rgb(color, 0, 0));
+    }
+
+    /**
+     * Initializes values for start & end date, start & end time, and date format.
+     */
+    public void setDateTime() {
+        date = tvDate.getText().toString().trim();
+        time = tvTime.getText().toString().trim();
+        DATEFORMAT = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm a");
+        start = LocalDateTime.parse(date + " " + time, DATEFORMAT);
+    }
+
+    /**
+     * Prompts user to select a date, and sets selected date in textView.
+     * The date can only be from the current date, or after selected start date.
+     *
+     * @param textView a TextView of the date.
+     */
+    public void setDate(TextView textView) {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                AddDeadlineEventActivity.this, (view, year, month, dayOfMonth) -> {
+            yr = year;
+            mth = month;
+            day = dayOfMonth;
+
+            @SuppressLint("DefaultLocale") String startDate = String.format("%02d-%02d-%d", day, (mth + 1), yr);
+            textView.setText(startDate);
+        }, yr, mth, day);
+
+        datePickerDialog.updateDate(yr, mth, day); // displays prev selected date
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis()); // disable past date selection
+        datePickerDialog.show();
+    }
+
+    /**
+     * Prompts user to select a time, and sets selected time in textView.
+     *
+     * @param textView a TextView of the time.
+     */
+    public void setTime(TextView textView) {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                AddDeadlineEventActivity.this, (view, hourOfDay, minute) -> {
+            hr = hourOfDay;
+            min = minute;
+
+            calendar.set(Calendar.DAY_OF_MONTH, day);
+            calendar.set(Calendar.HOUR_OF_DAY, hr);
+            calendar.set(Calendar.MINUTE, min);
+            textView.setText(DateFormat.format("hh:mm aa", calendar));
+        }, hr, min, false);
+        timePickerDialog.show();
+    }
+
+    /**
+     * Directs activity to the School activity on back arrow icon click.
+     *
+     * @param view a View for the device screen.
+     */
+    public void clickBack(View view) {
         activity.redirectActivity(this, SchoolActivity.class);
     }
 
-    //    Priority Selection
-    @Override
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
-        if (checkedId == R.id.rb_high_priority) {
-            priority = 1;
-        } else if (checkedId == R.id.rb_med_priority) {
-            priority = 2;
-        } else if (checkedId == R.id.rb_low_priority) {
-            priority = 3;
+    /**
+     * Saves event into the database.
+     *
+     * @param view a View for the device screen.
+     */
+    public void clickSaveEvent(View view) {
+        if (addEventInput()) {
+            String eventName = etEventName.getText().toString();
+            setDateTime();
+            LocalDateTime end = LocalDateTime.parse(endDate + " " + endTime, DATEFORMAT);
+
+            Event event = new Event(eventName, priority, start, end);
+            int userID = ug.getLoggedInUserID();
+
+            eg.saveToDatabase(event, userID);
+
+            activity.redirectActivity(this, SchoolActivity.class);
         }
+    }
+
+    /**
+     * Opens dialog for user to select a start date.
+     *
+     * @param view a View for the device screen.
+     */
+    public void clickStartDate(View view) {
+        setDate(tvDate);
+    }
+
+    /**
+     * Opens dialog for user to select an end time.
+     *
+     * @param view a View for the device screen.
+     */
+    public void clickStartTime(View view) {
+        setTime(tvTime);
     }
 }
