4ec43783d3aa094d83d4b08e3eec701bcf66516a
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-07 18:11:51 -0500

    fix: updated EventDateComparator to reflect change to SchoolEvent

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/UseCases/EventDateComparator.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/UseCases/EventDateComparator.java
@@ -4,20 +4,21 @@ import androidx.annotation.NonNull;
 
 import java.util.Comparator;
 
-import com.generic.plannr.Entities.Event;
+import com.generic.plannr.Entities.SchoolEvent;
 
 /**
  * A comparator to compare events by their date
  */
-public class EventDateComparator implements Comparator<Event> {
+public class EventDateComparator implements Comparator<SchoolEvent> {
     /**
      * Compares two events by their date.
+     *
      * @param e1 represents an Event
      * @param e2 represents an other Event
      * @return -1 if e1 before after e2, 0 if e1 and e2 occur at the same date-time,
      * and 1 if e1 after e2.
      */
-    public int compare(@NonNull Event e1, @NonNull Event e2) {
+    public int compare(@NonNull SchoolEvent e1, @NonNull SchoolEvent e2) {
         return Integer.compare(e1.getStartDate().compareTo(e2.getStartDate()), 0);
     }
 }
