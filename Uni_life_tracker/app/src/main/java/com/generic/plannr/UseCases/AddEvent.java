e0858bc0dbe6d3dc0300d7b8d79c4e4a3cc727c3
cdw18 <b.amoussou@mail.utoronto.ca>
2021-12-07 14:00:58 -0500

    docs: added javadocs to AddEvent

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/UseCases/AddEvent.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/UseCases/AddEvent.java
@@ -8,17 +8,40 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 /**
- * Adds an event to a user's event lists
+ * Adds a school event to a user's event lists
  */
 public class AddEvent {
 
-    public static void addSchoolEvent (@NonNull UserManager userManager, String eventType, String name, int priority,
-                                       LocalDateTime startDate,
+    /**
+     * Creates and adds a school event to a user's list of events
+     *
+     * @param userManager manager for user whose list of events were adding a school event
+     * @param eventType the type of event to be created and added ("deadline" or "assessment")
+     * @param name the name of the event to be created
+     * @param priority the priority of the event (0 = high, 1 = mid, 2 = low)
+     * @param startDate the start date-time of the event
+     * @param endDate the end date-time of the event
+     * @param course the course the event is for
+     */
+    public static void addSchoolEvent (@NonNull UserManager userManager, String eventType,
+                                       String name, int priority, LocalDateTime startDate,
                                        LocalDateTime endDate, String course) {
         userManager.addEventToUsersList(new SchoolEvent(eventType, name, priority, startDate,
                 endDate, course));
     }
 
+    /**
+     * Creates and adds a school event to a user's list of events
+     *
+     * @param userManager manager for user whose list of events were adding a school event
+     * @param eventType the type of event to be created and added ("class" or "study session")
+     * @param name the name of the event to be created
+     * @param priority the priority of the event (0 = high, 1 = mid, 2 = low)
+     * @param startDate the start date-time of the event
+     * @param endDate the end date-time of the event
+     * @param course the course the event is for
+     * @param location the location the event is taking place
+     */
     public static void addSchoolEvent (@NonNull UserManager userManager, String eventType,
                                        String name, int priority, LocalDateTime startDate,
                                        LocalDateTime endDate, String course, String location) {
