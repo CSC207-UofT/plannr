bc35b2e4cc3eaae43bcbc71bba44c1cdce7f83ae
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-07 18:10:51 -0500

    style: deleted unused imports and reformatted AddEvent

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/UseCases/AddEvent.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/UseCases/AddEvent.java
@@ -5,7 +5,6 @@ import androidx.annotation.NonNull;
 import com.generic.plannr.Entities.SchoolEvent;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 /**
  * Adds a school event to a user's event lists
@@ -16,16 +15,16 @@ public class AddEvent {
      * Creates and adds a school event to a user's list of events
      *
      * @param userManager manager for user whose list of events were adding a school event
-     * @param eventType the type of event to be created and added ("deadline" or "assessment")
-     * @param name the name of the event to be created
-     * @param priority the priority of the event (0 = high, 1 = mid, 2 = low)
-     * @param startDate the start date-time of the event
-     * @param endDate the end date-time of the event
-     * @param course the course the event is for
+     * @param eventType   the type of event to be created and added ("deadline" or "assessment")
+     * @param name        the name of the event to be created
+     * @param priority    the priority of the event (0 = high, 1 = mid, 2 = low)
+     * @param startDate   the start date-time of the event
+     * @param endDate     the end date-time of the event
+     * @param course      the course the event is for
      */
-    public static void addSchoolEvent (@NonNull UserManager userManager, String eventType,
-                                       String name, int priority, LocalDateTime startDate,
-                                       LocalDateTime endDate, String course) {
+    public static void addSchoolEvent(@NonNull UserManager userManager, String eventType,
+                                      String name, int priority, LocalDateTime startDate,
+                                      LocalDateTime endDate, String course) {
         userManager.addEventToUsersList(new SchoolEvent(eventType, name, priority, startDate,
                 endDate, course));
     }
@@ -34,17 +33,17 @@ public class AddEvent {
      * Creates and adds a school event to a user's list of events
      *
      * @param userManager manager for user whose list of events were adding a school event
-     * @param eventType the type of event to be created and added ("class" or "study session")
-     * @param name the name of the event to be created
-     * @param priority the priority of the event (0 = high, 1 = mid, 2 = low)
-     * @param startDate the start date-time of the event
-     * @param endDate the end date-time of the event
-     * @param course the course the event is for
-     * @param location the location the event is taking place
+     * @param eventType   the type of event to be created and added ("class" or "study session")
+     * @param name        the name of the event to be created
+     * @param priority    the priority of the event (0 = high, 1 = mid, 2 = low)
+     * @param startDate   the start date-time of the event
+     * @param endDate     the end date-time of the event
+     * @param course      the course the event is for
+     * @param location    the location the event is taking place
      */
-    public static void addSchoolEvent (@NonNull UserManager userManager, String eventType,
-                                       String name, int priority, LocalDateTime startDate,
-                                       LocalDateTime endDate, String course, String location) {
+    public static void addSchoolEvent(@NonNull UserManager userManager, String eventType,
+                                      String name, int priority, LocalDateTime startDate,
+                                      LocalDateTime endDate, String course, String location) {
         userManager.addEventToUsersList(new SchoolEvent(eventType, name, priority, startDate,
                 endDate, course, location));
     }
