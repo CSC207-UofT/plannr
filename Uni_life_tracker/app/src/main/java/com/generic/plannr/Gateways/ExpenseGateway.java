5d942c352382d1ea58622a65a9a9ad50d230d893
sarihammad <sari_hammad@hotmail.com>
2021-12-06 23:40:54 -0500

    style: added comments to demonstrate where Façade design pattern is used

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/Gateways/ExpenseGateway.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/Gateways/ExpenseGateway.java
@@ -31,6 +31,7 @@ public class ExpenseGateway implements ExpenseGatewayInterface {
      * Open the database for reading or writing
      */
     public void openDatabase() {
+        // Opens the database. (Note: this is an example usage of the Façade Design Pattern)
         db = dbclient.getWritableDatabase();
     }
 
