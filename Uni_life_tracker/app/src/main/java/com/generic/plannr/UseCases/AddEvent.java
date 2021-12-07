cc9b6822d1e69458eae6c397b3731988862b0201
cdw18 <b.amoussou@mail.utoronto.ca>
2021-12-07 13:56:44 -0500

    feat: implemented AddEvent to add SchoolEvent objects
    BREAKING CHANGE: any class that depended on old AddEvent methods will have an error

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/UseCases/AddEvent.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/UseCases/AddEvent.java
@@ -2,6 +2,8 @@ package com.generic.plannr.UseCases;
 
 import androidx.annotation.NonNull;
 
+import com.generic.plannr.Entities.SchoolEvent;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
@@ -10,70 +12,17 @@ import java.util.ArrayList;
  */
 public class AddEvent {
 
-    /**
-     * Adds a Deadline event to a user's event list
-     * @param userManager is the user manager of the user
-     * @param name is the name of this deadline event
-     * @param priority is the priority of this deadline event
-     * @param endDate is the due date of this deadline event
-     * @param course is the course this deadline is for
-     */
-    public static void addDeadline(@NonNull UserManager userManager, String name, int priority,
-                                   LocalDateTime endDate, String course) {
-
-        userManager.addEventToUsersList(new Deadline(name, priority, endDate, course));
+    public static void addSchoolEvent (@NonNull UserManager userManager, String eventType, String name, int priority,
+                                       LocalDateTime startDate,
+                                       LocalDateTime endDate, String course) {
+        userManager.addEventToUsersList(new SchoolEvent(eventType, name, priority, startDate,
+                endDate, course));
     }
 
-    /**
-     * Adds an Assessment event to a user's event list
-     * @param userManager is the user manager of the user
-     * @param name is the name of the assessment
-     * @param priority is the priority of the assessment
-     * @param startDate is the start date of the assessment
-     * @param endDate is the end date of the assessment
-     * @param course is the course this assessment is for
-     */
-    public static void addAssessment(@NonNull UserManager userManager, String name,
-                                     int priority, LocalDateTime startDate,
-                                     LocalDateTime endDate, String course) {
-
-        userManager.addEventToUsersList(new Assessment (name, priority, startDate, endDate, course));
-    }
-
-    /**
-     * Adds a StudySession event to a user's event list
-     * @param userManager is the user manager of the user
-     * @param name is the name of the study session event
-     * @param priority is the priority of this study session event
-     * @param startDate is the start date of the study session
-     * @param endDate is the end date of the study session
-     * @param course is the course this study session is for
-     * @param location is the location, or where, this study session is taking place
-     * @param participants is the list of participants for this study session
-     */
-    public static void addStudySession(@NonNull UserManager userManager, String name,
-                                       int priority, LocalDateTime startDate, LocalDateTime endDate,
-                                       String course, String location,
-                                       ArrayList<String> participants) {
-
-        userManager.addEventToUsersList(new StudySession (name, priority, startDate, endDate,
-                course, location, participants));
-    }
-
-    /**
-     * Adds a Class event to a user's event list
-     * @param userManager is the user manager of the user
-     * @param name is the name of this class event
-     * @param priority is the priority of this class event
-     * @param startDate is the start date of the class
-     * @param endDate is the end date of this class
-     * @param course is the course this class event is for
-     * @param location is the location, or where, this class is taking place
-     */
-    public static void addClass(@NonNull UserManager userManager, String name, int priority,
-                                LocalDateTime startDate, LocalDateTime endDate, String course,
-                                String location) {
-        userManager.addEventToUsersList(new Class (name, priority, startDate,
+    public static void addSchoolEvent (@NonNull UserManager userManager, String eventType,
+                                       String name, int priority, LocalDateTime startDate,
+                                       LocalDateTime endDate, String course, String location) {
+        userManager.addEventToUsersList(new SchoolEvent(eventType, name, priority, startDate,
                 endDate, course, location));
     }
 }
