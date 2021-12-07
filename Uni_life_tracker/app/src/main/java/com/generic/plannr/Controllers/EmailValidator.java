36760b447455040befa34092de0c3be923d3fd43
sarihammad <sari_hammad@hotmail.com>
2021-12-07 07:51:17 -0500

    refactor: added missing javadocs

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/Controllers/EmailValidator.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/Controllers/EmailValidator.java
@@ -8,8 +8,16 @@ import com.google.android.material.textfield.TextInputLayout;
 import java.util.Objects;
 
 public class EmailValidator implements Validator {
-
-
+    /**
+     * Validates inputs and displays the different error messages for the user inpupts
+     *
+     * @param userInput  the input from the main layout that will display the error
+     * @param ug         the gateway connecting to the userinfo database
+     * @param tiEmail    the user's email
+     * @param tiPassword the user's password
+     * @param isSignup   whether validation is for signup or login
+     * @return whether the user input is valid and sets an error message if needed
+     */
     public boolean validateEntry(TextInputLayout userInput, UserGateway ug, TextInputLayout tiEmail,
                                  TextInputLayout tiPassword, boolean isSignup) {
         String input = Objects.requireNonNull(userInput.getEditText()).getText().toString().trim();
@@ -21,8 +29,9 @@ public class EmailValidator implements Validator {
         } else if (isSignup && userInput == tiEmail && !Patterns.EMAIL_ADDRESS.matcher(input).matches()) {
             userInput.setError("Please enter a valid email address");
             return false;
-        } else if(isSignup && userInput == tiEmail && !ug.uniqueEmail(input)){
-            userInput.setError("This email is already being used");return false;
+        } else if (isSignup && userInput == tiEmail && !ug.uniqueEmail(input)) {
+            userInput.setError("This email is already being used");
+            return false;
 
         } else if (!isSignup && userInput == tiEmail && ug.uniqueEmail(email)) {
             userInput.setError("The email you entered does not belong to any account");
--- Uni_life_tracker/app/src/main/java/com/generic/plannr/Controllers/InputTextValidator.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/Controllers/InputTextValidator.java
@@ -12,19 +12,17 @@ public class InputTextValidator implements Validator {
     /**
      * Validates inputs and displays the different error messages for the user inpupts
      *
-     * @param userInput the input from the main layout that will display the error
-     * @param ug the gateway connecting to the userinfo database
-
-     * @param isSignup whether validation is for signup or login
-     *
+     * @param userInput  the input from the main layout that will display the error
+     * @param ug         the gateway connecting to the userinfo database
+     * @param tiEmail    the user's email
+     * @param tiPassword the user's password
+     * @param isSignup   whether validation is for signup or login
      * @return whether the user input is valid and sets an error message if needed
-     *
      */
     public boolean validateEntry(TextInputLayout userInput, UserGateway ug, TextInputLayout tiEmail,
                                  TextInputLayout tiPassword, boolean isSignup) {
         String input = Objects.requireNonNull(userInput.getEditText()).getText().toString().trim();
         String email = Objects.requireNonNull(tiEmail.getEditText()).getText().toString();
-
 
         if (input.isEmpty()) {
             userInput.setError("Field cannot be empty");
@@ -41,12 +39,10 @@ public class InputTextValidator implements Validator {
      * Validates inputs of events to make sure input is not empty
      *
      * @param userInput the input the user inputs into the textview
-     *
-     * @return          whether the user input is valid and sets an error message if needed
+     * @return whether the user input is valid and sets an error message if needed
      */
     public boolean validateAddEvent(TextView userInput) {
-        if (userInput.getText().toString().matches(""))
-        {
+        if (userInput.getText().toString().matches("")) {
             userInput.setHintTextColor(Color.RED);
             return false;
         } else {
