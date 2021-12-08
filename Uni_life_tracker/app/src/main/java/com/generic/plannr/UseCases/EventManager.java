69ceb6ef195524bc47dc25e4c2e2f59e20565f70
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-07 20:16:21 -0500

    feat: implemented private constructor of EventManager

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/UseCases/EventManager.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/UseCases/EventManager.java
@@ -4,6 +4,13 @@ import com.generic.plannr.Entities.SchoolEvent;
 import com.generic.plannr.Entities.User;
 
 public class EventManager {
+    EventManager EM_INSTANCE;
+
+    /**
+     * This is a private constructor, implemented to disallow client to use the new keyword
+     */
+    private EventManager() {
+    }
 
     /**
      * Add an com.generic.plannr.Entities.Event
