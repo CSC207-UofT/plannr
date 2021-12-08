6d4308fc4ceac1aeeb05a5bbbe60deeaf11040de
cdw18 <b.amoussou@mail.utoronto.ca>
2021-12-08 15:20:25 -0500

    feat: implemented EventList

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/UseCases/EventList.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/UseCases/EventList.java
@@ -1,5 +1,27 @@
 package com.generic.plannr.UseCases;
 
 
+import com.generic.plannr.Entities.Event;
+import com.generic.plannr.Entities.SchoolEvent;
+
+import java.util.ArrayList;
+
+/**
+ * A data structure for the current user's list of events
+ */
 public class EventList {
+    private final ArrayList<Event> eventList = new ArrayList<>();
+
+    public ArrayList<Event> getEventList() {
+        return eventList;
+    }
+
+    public void add (Event e) {
+        eventList.add(e);
+    }
+
+
+    public void clear() {
+        eventList.clear();
+    }
 }
