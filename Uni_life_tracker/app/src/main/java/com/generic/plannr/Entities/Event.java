b8866207aef9bdec4195e1949bb345fa11c67800
cdw18 <b.amoussou@mail.utoronto.ca>
2021-12-08 14:54:49 -0500

    docs: added javadocs to abstract class Event and its methods

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/Entities/Event.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/Entities/Event.java
@@ -4,16 +4,25 @@ import java.time.LocalDateTime;
 
 public abstract class Event {
     private final int userID;
-    private final String eventType;
     private final String name;
     private final int priority;
     private final LocalDateTime startDate;
     private final LocalDateTime endDate;
 
-    public Event(int userID, String eventType, String name, int priority,
+
+     /**
+     * Construct an Event, giving them the given
+     * name, priority, start date, end date, course.
+     *
+     * @param userID the userID of the user this event belongs to
+     * @param name This Event's name
+     * @param priority This Event's priority (0 = high, 1 = mid, 2 = low)
+     * @param startDate This Event's start date
+     * @param endDate This Event's end date
+      */
+    public Event(int userID, String name, int priority,
                  LocalDateTime startDate, LocalDateTime endDate) {
         this.userID = userID;
-        this.eventType = eventType;
         this.name = name;
         this.priority = priority;
         this.startDate = startDate;
@@ -21,15 +30,33 @@ public abstract class Event {
 
     }
 
+    /**
+     * Returns the userID of the user this event belongs to
+     * @return the userID of the Event
+     */
     public int getUserID() {return this.userID; }
 
-    public String getEventType() { return this.eventType; }
-
+    /**
+     * Returns the name of this event
+     * @return the name of the Event
+     */
     public String getName() { return this.name; }
 
+    /**
+     * Returns the priority (0 = high, 1 = mid, 2 = low) of this event
+     * @return the priority of the Event
+     */
     public int getPriority() { return this.priority; }
 
+    /**
+     * Returns the start date of this event
+     * @return the start date of the Event
+     */
     public LocalDateTime getStartDate() { return this.startDate; }
 
+    /**
+     * Returns the end date of this event
+     * @return the end date of the Event
+     */
     public LocalDateTime getEndDate() { return this.endDate; }
 }
