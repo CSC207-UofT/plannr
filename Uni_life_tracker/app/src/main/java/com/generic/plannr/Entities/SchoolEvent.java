5130648090ed1ba7bc0302f376945aca05bca521
cdw18 <b.amoussou@mail.utoronto.ca>
2021-12-08 14:56:11 -0500

    docs: added javadocs to SchoolEvent and its methods refactor: made event type a SchoolEvent variable

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/Entities/SchoolEvent.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/Entities/SchoolEvent.java
@@ -42,21 +42,27 @@ public class SchoolEvent {
      * Construct an com.generic.plannr.Entities.SchoolEvent event, giving them the given
      * name, priority, start date, end date, course.
      *
-     * @param eventType This com.generic.plannr.Entities.SchoolEvent's event type ("assessment",
+     * @param eventType This SchoolEvent's event type ("assessment",
      *                  "deadline", "class", "study session")
      * @param name This com.generic.plannr.Entities.SchoolEvent's name
      * @param priority This com.generic.plannr.Entities.SchoolEvent's priority
      *                 (0 = high, 1 = mid, 2 = low)
-     * @param startDate This com.generic.plannr.Entities.SchoolEvent's start date
-     * @param endDate This com.generic.plannr.Entities.SchoolEvent's end date
-     * @param course This com.generic.plannr.Entities.SchoolEvent's course
-     * @param location This com.generic.plannr.Entities.SchoolEvent's location
+     * @param startDate This SchoolEvent's start date
+     * @param endDate This SchoolEvent's end date
+     * @param course This SchoolEvent's course
+     * @param location This SchoolEvent's location
      */
     public SchoolEvent(int userID, String eventType, String name, int priority, LocalDateTime startDate,
                        LocalDateTime endDate, String course, String location) {
         this (userID, eventType, name, priority, startDate, endDate, course);
         this.location = location;
     }
+
+    /**
+     * Returns the event type of this event
+     * @return the event type of the Event
+     */
+    public String getEventType() { return this.eventType; }
 
     /**
      * Gets the course of SchoolEvent
