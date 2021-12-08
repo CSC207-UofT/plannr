39c1368818e1370e0d7f39989eaed038a0e3234d
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-07 20:19:46 -0500

    fix: fixed scope of EM_INSTANCE

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/UseCases/EventManager.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/UseCases/EventManager.java
@@ -4,7 +4,7 @@ import com.generic.plannr.Entities.SchoolEvent;
 import com.generic.plannr.Entities.User;
 
 public class EventManager {
-    EventManager EM_INSTANCE;
+    private static EventManager EM_INSTANCE;
 
     /**
      * This is a private constructor, implemented to disallow client to use the new keyword
