b6ec179831d85c7edcf45c6b1634da3d9e3719da
Kathy Lee <kathyam.lee@mail.utoronto.ca>
2021-12-08 06:30:21 -0500

    fix: fix implementation in school view
    Authored-by: Dana Alshekerchi <d.alshekerchi@mail.utoronto.ca> <dalshekerchi>

--- Uni_life_tracker/app/src/main/java/com/generic/plannr/SchoolActivity.java
+++ Uni_life_tracker/app/src/main/java/com/generic/plannr/SchoolActivity.java
@@ -69,10 +69,12 @@ public class SchoolActivity extends AppCompatActivity implements CalendarAdapter
             DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm a");
             AlertDialog.Builder builder = new AlertDialog.Builder(v.getRootView().getContext());
             View dialogView;
+            String startDate = eventsList.get(position).getStartDate().toLocalDate().format(dayFormatter);
+            String startTime = eventsList.get(position).getStartDate().toLocalTime().format(timeFormatter);
+
             switch (eventsList.get(position).getEventType()) {
                 case "Assessment":
                     dialogView = LayoutInflater.from(v.getRootView().getContext()).inflate(R.layout.popup_view_event, null);
-
                     dialogEventName = dialogView.findViewById(R.id.tv_event_name_pop);
                     dialogEventCourse = dialogView.findViewById(R.id.tv_course);
                     dialogEventStartD = dialogView.findViewById(R.id.tv_start_date);
@@ -80,7 +82,8 @@ public class SchoolActivity extends AppCompatActivity implements CalendarAdapter
                     dialogEventEndD = dialogView.findViewById(R.id.tv_end_date);
                     dialogEventEndT = dialogView.findViewById(R.id.tv_end_time);
                     dialogEventPriority = dialogView.findViewById(R.id.tv_priority);
-
+                    dialogEventStartD.setText(startDate);
+                    dialogEventStartT.setText(startTime);
                     break;
                 case "Deadline":
                     dialogView = LayoutInflater.from(v.getRootView().getContext()).inflate(R.layout.popup_deadline_event, null);
@@ -101,9 +104,9 @@ public class SchoolActivity extends AppCompatActivity implements CalendarAdapter
                     dialogEventEndT = dialogView.findViewById(R.id.tv_end_time_l);
                     dialogEventPriority = dialogView.findViewById(R.id.tv_priority_l);
                     dialogEventLocation = dialogView.findViewById(R.id.tv_location);
-
-
-                    break;
+                    dialogEventLocation.setText(eventsList.get(position).getLocation());
+                    dialogEventStartD.setText(startDate);
+                    dialogEventStartT.setText(startTime);
             }
 
             String endDate = eventsList.get(position).getEndDate().toLocalDate().format(dayFormatter);
@@ -122,12 +125,6 @@ public class SchoolActivity extends AppCompatActivity implements CalendarAdapter
                     break;
             }
 
-
-            String startDate = eventsList.get(position).getStartDate().toLocalDate().format(dayFormatter);
-            String startTime = eventsList.get(position).getStartDate().toLocalTime().format(timeFormatter);
-            dialogEventStartD.setText(startDate);
-            dialogEventStartT.setText(startTime);
-            dialogEventLocation.setText(eventsList.get(position).getLocation());
             dialogEventName.setText(eventsList.get(position).getName());
             dialogEventCourse.setText(eventsList.get(position).getCourse());
             dialogEventEndD.setText(endDate);
