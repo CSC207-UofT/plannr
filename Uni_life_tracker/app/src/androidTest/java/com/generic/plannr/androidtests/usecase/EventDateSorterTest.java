70cc157d912a9d654522b354e3bb5c8899db58c3
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2021-12-08 21:46:53 -0500

    test: implemented test for EventDateSorter

---
+++ Uni_life_tracker/app/src/androidTest/java/com/generic/plannr/androidtests/usecase/EventDateSorterTest.java
@@ -0,0 +1,96 @@
+package com.generic.plannr.androidtests.usecase;
+
+import static org.junit.Assert.*;
+import static com.generic.plannr.UseCases.UserCreator.signUp;
+import static com.generic.plannr.UseCases.AddSchoolEvent.addSchoolEvent;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.generic.plannr.Entities.SchoolEvent;
+import com.generic.plannr.Gateways.EventGateway;
+import com.generic.plannr.Gateways.UserGateway;
+import com.generic.plannr.UseCases.AddSchoolEvent;
+import com.generic.plannr.UseCases.EventDateComparator;
+import com.generic.plannr.UseCases.EventDateSorter;
+import com.generic.plannr.UseCases.EventList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class EventDateSorterTest {
+    Context context;
+    EventList eventList;
+    EventGateway EG;
+    UserGateway UG;
+    AddSchoolEvent AS_INSTANCE;
+    Random rand;
+    EventDateComparator EDC;
+    EventDateSorter EDS;
+
+    @Before
+    public void setUp() {
+        context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        EG = new EventGateway(context);
+        EG.openDatabase();
+        UG = new UserGateway(context);
+        UG.openDatabase();
+        signUp(
+                UG,
+                "test",
+                "test@gmail.com",
+                "Test@123"
+        );
+        eventList = new EventList();
+        rand = new Random();
+        AS_INSTANCE = new AddSchoolEvent(eventList, EG);
+        EDC = new EventDateComparator();
+        EDS = new EventDateSorter(EDC);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        context.deleteDatabase("PlannrDatabase");
+    }
+
+    @Test
+    public void TestCompare() {
+        int iteration = rand.nextInt(20) + 5;
+        ArrayList<LocalDateTime> expectedList = new ArrayList<>();
+
+        for (int i = 0; i < iteration; i++) {
+            LocalDateTime time = LocalDateTime.of(2021,
+                    rand.nextInt(11) + 1,
+                    rand.nextInt(27) + 1,
+                    0,
+                    0,
+                    0);
+            expectedList.add(time);
+            addSchoolEvent(
+                    UG.getLoggedInUserID(),
+                    "assessment",
+                    "test event",
+                    rand.nextInt(2),
+                    time,
+                    time,
+                    "CSC207"
+            );
+        }
+
+        Collections.sort(expectedList);
+
+        ArrayList<SchoolEvent> sortedEventList = EDS.sortEvents(EG.getAllEvents(UG.getLoggedInUserID()));
+
+        for (int i = 0; i < iteration; i++) {
+            assertEquals(expectedList.get(i), sortedEventList.get(i).getStartDate());
+        }
+
+    }
+}
